8ae46f3b87194cbe9418fe4838a3b0f90950909c
Mostafa Soufi <mostafa.soufi@hotmail.com>
2017-12-04 21:12:27 +0330

    Update ebulksms gateway

--- includes/gateways/_ebulksms.class.php
+++ includes/gateways/_ebulksms.class.php
@@ -2,103 +2,110 @@
 
 class _ebulksms extends WP_SMS {
 
-    public $tariff = "http://api.ebulksms.com:8080/";
-    public $unitrial = true;
-    public $unit;
-    public $flash = "enable";
-    public $isflash = false;
+	public $wsdl_link = "http://api.ebulksms.com";
+	public $tariff = "http://ebulksms.com/";
+	public $unitrial = true;
+	public $unit;
+	public $flash = "enable";
+	public $isflash = false;
 
-    public function __construct() {
-        parent::__construct();
-        $this->validateNumber = "2347030000000,2348020000000,23489010000000";
+	public function __construct() {
+		parent::__construct();
+		$this->validateNumber = "2347030000000,2348020000000,23489010000000";
 
-        // Enable api key
-        $this->has_key = true;
+		// Enable api key
+		$this->has_key = true;
+	}
 
-        // includes library
-        include( 'includes/ebulksms/ebulksms.class.php' );
-    }
+	public function SendSMS() {
+		// Check gateway credit
+		if ( is_wp_error( $this->GetCredit() ) ) {
+			return new WP_Error( 'account-credit', __( 'Your account has no credit for sending sms.', 'wp-sms' ) );
+		}
 
-    public function SendSMS() {
-        // Check gateway credit
-        if (is_wp_error($this->GetCredit())) {
-            return new WP_Error('account-credit', __('Your account has no credit for sending sms.', 'wp-sms'));
-        }
+		/**
+		 * Modify sender number
+		 *
+		 * @since 3.4
+		 *
+		 * @param string $this ->from sender number.
+		 */
+		$this->from = apply_filters( 'wp_sms_from', $this->from );
 
-        /**
-         * Modify sender number
-         *
-         * @since 3.4
-         *
-         * @param string $this ->from sender number.
-         */
-        $this->from = apply_filters('wp_sms_from', $this->from);
+		/**
+		 * Modify Receiver number
+		 *
+		 * @since 3.4
+		 *
+		 * @param array $this ->to receiver number
+		 */
+		$this->to = apply_filters( 'wp_sms_to', $this->to );
 
-        /**
-         * Modify Receiver number
-         *
-         * @since 3.4
-         *
-         * @param array $this ->to receiver number
-         */
-        $this->to = apply_filters('wp_sms_to', $this->to);
+		/**
+		 * Modify text message
+		 *
+		 * @since 3.4
+		 *
+		 * @param string $this ->msg text message.
+		 */
+		$this->msg = apply_filters( 'wp_sms_msg', $this->msg );
 
-        /**
-         * Modify text message
-         *
-         * @since 3.4
-         *
-         * @param string $this ->msg text message.
-         */
-        $this->msg = apply_filters('wp_sms_msg', $this->msg);
+		$response = wp_remote_get( $this->wsdl_link . "/sendsms?username=" . $this->username . "&apikey=" . $this->has_key . "&sender=" . $this->from . "&messagetext=" . urlencode( $this->msg ) . "&flash=0&recipients=" . implode( ',', $this->to ) );
 
-        // Init class
-        $ebulksms = new Ebulksms($this->username, $this->has_key);
+		// Check gateway credit
+		if ( is_wp_error( $response ) ) {
+			return new WP_Error( 'send-sms', $response->get_error_message() );
+		}
 
-        // Add recipients
-        $ebulksms->add_recipients($this->to);
+		// Ger response code
+		$response_code = wp_remote_retrieve_response_code( $response );
 
-        // Set From Name
-        $ebulksms->set_from($this->from);
+		// Check response code
+		if ( $response_code == '200' ) {
+			if ( strpos( $response['body'], 'SUCCESS' ) !== false ) {
+				$this->InsertToDB( $this->from, $this->msg, $this->to );
 
-        // Set Message
-        $ebulksms->set_message($this->msg);
+				/**
+				 * Run hook after send sms.
+				 *
+				 * @since 2.4
+				 *
+				 * @param string $response result output.
+				 */
+				do_action( 'wp_sms_send', $response );
 
-        // Send sms
-        $result = $ebulksms->send();
+				return $response;
+			} else {
+				return new WP_Error( 'send-sms', $response['body'] );
+			}
 
-        // Check result
-        if (!$result) {
-            return new WP_Error('send-sms', $result);
-        }
+		} else {
+			return new WP_Error( 'send-sms', $response['body'] );
+		}
+	}
 
-        $this->InsertToDB($this->from, $this->msg, $this->to);
+	public function GetCredit() {
+		// Check username and password
+		if ( ! $this->username && ! $this->has_key ) {
+			return new WP_Error( 'account-credit', __( 'Username/Password was not set for this gateway', 'wp-sms' ) );
+		}
 
-        /**
-         * Run hook after send sms.
-         *
-         * @since 2.4
-         *
-         * @param string $result result output.
-         */
-        do_action('wp_sms_send', $result);
+		// Get response
+		$response = wp_remote_get( $this->wsdl_link . '/balance/' . $this->username . '/' . $this->has_key );
 
-        return $result;
-    }
+		// Check gateway credit
+		if ( is_wp_error( $response ) ) {
+			return new WP_Error( 'account-credit', $response->get_error_message() );
+		}
 
-    public function GetCredit() {
-        // Check username and password
-        if (!$this->username && !$this->has_key) {
-            return new WP_Error('account-credit', __('Username/Password was not set for this gateway', 'wp-sms'));
-        }
+		$response_code = wp_remote_retrieve_response_code( $response );
 
-        // Init class
-        $ebulksms = new Ebulksms($this->has_key);
+		if ( $response_code == '200' ) {
+			return $response['body'];
 
-        // Get credit
-        $credits = $ebulksms->get_credits($this->username, $this->has_key);
-
-        return $credits;
-    }
+		} else {
+			return new WP_Error( 'account-credit', $response['body'] );
+		}
+	}
 
 }
