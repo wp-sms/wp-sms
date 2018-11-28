7975f185dc257a2418926cffdae684be3f901044
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-11-28 16:27:51 +0330

    Minor improvements.

--- wp-sms.php
+++ wp-sms.php
@@ -302,9 +302,10 @@ class WP_SMS_Plugin {
 	 */
 	public function front_assets() {
 		global $wpsms_option;
-		wp_register_style( 'wpsms-subscribe', plugin_dir_url( __FILE__ ) . 'assets/css/subscribe.css', true, '1.1' );
-		//Check if Disable Style in frontend is active or not
-		if ( ! isset( $wpsms_option['disable_style_in_front'] ) and ! $wpsms_option['disable_style_in_front'] ) {
+
+		// Check if Disable Style in frontend is active or not
+		if ( empty( $wpsms_option['disable_style_in_front'] ) or ( isset( $wpsms_option['disable_style_in_front'] ) and ! $wpsms_option['disable_style_in_front'] ) ) {
+			wp_register_style( 'wpsms-subscribe', plugin_dir_url( __FILE__ ) . 'assets/css/subscribe.css', true, '1.1' );
 			wp_enqueue_style( 'wpsms-subscribe' );
 		}
 	}
