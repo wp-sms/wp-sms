8093444c68c0fe159c4a267e2e8edef15d4d0276
MHitMaN <ghasemi71ir@gmail.com>
2019-01-15 09:21:48 +0330

    Update: Recode 'Primotexto' gateway

--- includes/gateways/class-wpsms-gateway-primotexto.php
+++ includes/gateways/class-wpsms-gateway-primotexto.php
@@ -15,8 +15,6 @@ class primotexto extends \WP_SMS\Gateway {
 		$this->validateNumber = "Format: 0600000000, +33600000000";
 		$this->help           = 'Vous devez génerer une clé depuis votre <a href="https://www.primotexto.com/webapp/#/developer/keys">interface Primotexto</a> pour pouvoir utiliser l\'API.';
 		$this->has_key        = true;
-		//$this->bulk_send      = false;
-		require_once 'libraries/primotexto/baseManager.class.php';
 	}
 
 	public function SendSMS() {
@@ -59,49 +57,85 @@ class primotexto extends \WP_SMS\Gateway {
 			return $credit;
 		}
 
+		$api  = $this->has_key;
+		$to   = implode( ',', $this->to );
+		$msg  = $this->msg;
+		$from = $this->from;
 		// Authentication
-		\authenticationManager::setApiKey( $this->has_key );
+		$args = array(
+			'headers' => array(
+				'X-Primotexto-ApiKey' => $api,
+				'Content-Type'        => 'application/json',
+			),
+			'body'    => json_encode(
+				array(
+					'number'  => $to,
+					'message' => $msg,
+					'sender'  => $from
+				) )
+		);
 
-		// New notification SMS
-		foreach ( $this->to as $item ) {
-			$sms          = new Sms;
-			$sms->type    = 'notification';
-			$sms->number  = $item;
-			$sms->message = urlencode( $this->msg );
-			$sms->sender  = $this->from;
+		$response = wp_remote_post( $this->wsdl_link . "notification/messages/send", $args );
 
-			$result = \messagesManager::messagesSend( $sms );
-			$json   = json_decode( $result );
-		}
-
-		if ( isset( $json->snapshotId ) ) {
+		// check response have error or not
+		if ( is_wp_error( $response ) ) {
 			// Log the result
-			$this->log( $this->from, $this->msg, $this->to, $json );
+			$this->log( $this->from, $this->msg, $this->to, $response->get_error_message(), 'error' );
 
-			/**
-			 * Run hook after send sms.
-			 *
-			 * @since 2.4
-			 *
-			 * @param string $result result output.
-			 */
-			do_action( 'wp_sms_send', $json );
-
-			return $json;
+			return new \WP_Error( 'send-sms', $response->get_error_message() );
 		}
-		// Log the result
-		$this->log( $this->from, $this->msg, $this->to, $json->code, 'error' );
 
-		return new \WP_Error( 'credit', $json->code );
+		// Ger response code
+		$response_code = wp_remote_retrieve_response_code( $response );
+
+		// Decode response
+		$response = json_decode( $response['body'] );
+
+		// Check response code
+		if ( $response_code == '200' ) {
+			if ( isset( $response->snapshotId ) ) {
+
+				// Log the result
+				$this->log( $this->from, $this->msg, $this->to, $response );
+
+				/**
+				 * Run hook after send sms.
+				 *
+				 * @since 2.4
+				 *
+				 * @param string $result result output.
+				 */
+				do_action( 'wp_sms_send', $response );
+
+				return $response;
+			}
+			// Log the result
+			$this->log( $this->from, $this->msg, $this->to, $response->code, 'error' );
+
+			return new \WP_Error( 'credit', $response->code );
+		} else {
+			// Log the result
+			$this->log( $this->from, $this->msg, $this->to, $response->code, 'error' );
+
+			return new \WP_Error( 'credit', $response->code );
+		}
 	}
 
 	public function GetCredit() {
+		// Check username and password
+		if ( ! $this->has_key ) {
+			return new \WP_Error( 'account-credit', __( 'API does not set for this gateway', 'wp-sms' ) );
+		}
 		// Authentication
-		\authenticationManager::setApiKey( $this->has_key );
+		$args = array(
+			'headers' => array(
+				'X-Primotexto-ApiKey' => $this->has_key,
+			)
+		);
 
-		// Account Stats
-		$result = \accountManager::accountStats();
-		$json   = json_decode( $result );
+		$result = wp_remote_get( $this->wsdl_link . "account/stats", $args );
+
+		$json = json_decode( $result['body'] );
 
 		if ( isset( $json->error ) ) {
 			return new \WP_Error( 'credit', $json->error );
