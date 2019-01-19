fe474570bac46fbff4cfaf3b0c9f83b5481a497a
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-01-19 11:02:44 +0330

    Add get option template function

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -1,5 +1,7 @@
 <?php
 
+use WP_SMS\Option;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
@@ -8,4 +10,15 @@ if ( ! function_exists( 'wp_subscribes' ) ) {
 	function wp_subscribes() {
 		Newsletter::loadNewsLetter();
 	}
+}
+
+/**
+ * @param $option_name
+ * @param bool $pro
+ * @param string $setting_name
+ *
+ * @return string
+ */
+function wp_sms_get_option($option_name, $pro = false, $setting_name = '') {
+	return Option::getOption($option_name, $pro, $setting_name);
 }
