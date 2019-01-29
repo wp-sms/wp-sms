9bfd503a425c01677eeeb225cca562b98b60533b
Kamran Khorsandi <kamran.khorsandi@gmail.com>
2019-01-29 16:41:26 +0330

    Add global send function

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -23,3 +23,18 @@ if ( ! function_exists( 'wp_subscribes' ) ) {
 function wp_sms_get_option($option_name, $pro = false, $setting_name = '') {
 	return Option::getOption($option_name, $pro, $setting_name);
 }
+
+/**
+ * @param $to
+ * @param $msg $pro
+ * @param $isflash
+ *
+ * @return string | WP_Error
+ */
+function wp_sms_send( $to, $msg, $isflash = false ) {
+	global $sms;
+	$sms->isflash = $isflash;
+	$sms->to  = array( $to );
+	$sms->msg = $msg;
+	return $sms->SendSMS();
+}
