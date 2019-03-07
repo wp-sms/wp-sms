d480888c2ba9b69ad32bdd130d4f0357e01c72b6
MHitMaN <ghasemi71ir@gmail.com>
2019-03-07 15:03:50 +0330

    Update Welcome page #68

--- includes/admin/welcome/welcome.php
+++ includes/admin/welcome/welcome.php
@@ -30,39 +30,26 @@
             <section class="normal-section">
                 <div class="left">
                     <div class="content-padding">
-                        <h2><?php _e( 'Online Users Widget', 'wp-sms' ); ?></h2>
-                        <p><?php _e( 'A cool widget to show current online users!', 'wp-sms' ); ?></p>
+                        <h2><?php _e( 'New Integration!', 'wp-sms' ); ?></h2>
+                        <p><?php _e( 'Now Ultimate Members plugin supporting for integration.', 'wp-sms' ); ?></p>
                     </div>
                 </div>
 
                 <div class="right text-center">
-                    <img src="<?php echo plugins_url( 'wp-sms/assets/images/welcome/usersonline-widget.png' ); ?>"/>
+                    <img src="<?php echo plugins_url( 'wp-sms/assets/images/welcome/what-is-new/ultimate-members.png' ); ?>"/>
                 </div>
             </section>
 
             <section class="normal-section">
                 <div class="right">
                     <div class="content-padding">
-                        <h2><?php _e( 'Top Referring Sites', 'wp-sms' ); ?></h2>
-                        <p><?php _e( 'Site icon, Server IP and minor improvements.', 'wp-sms' ); ?></p>
+                        <h2><?php _e( 'WooCommerce OTP', 'wp-sms' ); ?></h2>
+                        <p><?php _e( 'Mobile verification for submit a new orders with limitation and period time on retries.', 'wp-sms' ); ?></p>
                     </div>
                 </div>
 
                 <div class="left text-center">
-                    <img src="<?php echo plugins_url( 'wp-sms/assets/images/welcome/topreferring-widget.png' ); ?>"/>
-                </div>
-            </section>
-
-            <section class="normal-section">
-                <div class="left">
-                    <div class="content-padding">
-                        <h2><?php _e( 'Top 10 Counties', 'wp-sms' ); ?></h2>
-                        <p><?php _e( 'You can see all visitor of a country!', 'wp-sms' ); ?></p>
-                    </div>
-                </div>
-
-                <div class="right text-center">
-                    <img src="<?php echo plugins_url( 'wp-sms/assets/images/welcome/topcountry-widget.png' ); ?>"/>
+                    <img src="<?php echo plugins_url( 'wp-sms/assets/images/welcome/what-is-new/otp.png' ); ?>"/>
                 </div>
             </section>
 
@@ -75,12 +62,10 @@
 
                 <div class="right">
                     <ul>
-                        <li>Added new table `visitor_relationship` for saving visitors logs.</li>
-                        <li>Update database version.</li>
-                        <li>Improvement MySQL time query in all functions.</li>
-                        <li>Improvement CSV exporter.</li>
-                        <li>Improvement pagination in admin pages.</li>
-                        <li>Improvement time filter in admin pages stats.</li>
+                        <li>Update: dot4all.it gateway now available on free version.</li>
+                        <li>Update: gatewayapi.com to support Unicode.</li>
+                        <li>Improved: Response status and Credits to do not save result if is object.</li>
+                        <li>Improvments some gateways class.</li>
                     </ul>
                 </div>
             </section>
@@ -88,7 +73,7 @@
 
         <div data-content="pro" class="tab-content">
             <section class="center-section">
-				<?php include( WP_SMS_DIR . "includes/admin/welcome/welcome-add-ons.php" ); ?>
+				<?php include( WP_SMS_DIR . "includes/admin/welcome/welcome-pro-tab.php" ); ?>
             </section>
         </div>
 
@@ -121,9 +106,13 @@
                 <p class="clear"><?php echo sprintf( __( 'WP-SMS is being developed on GitHub, if you’re interested in contributing to the plugin, please look at the <a href="%s" target="_blank">GitHub page</a>.', 'wp-sms' ), 'https://github.com/veronalabs/wp-sms' ); ?></p>
                 <h3 class="wp-people-group"><?php _e( 'External Libraries', 'wp-sms' ); ?></h3>
                 <p class="wp-credits-list">
-                    <a target="_blank" href="https://maxmind.com/">MaxMind</a>,
-                    <a target="_blank" href="https://www.chartjs.org/">Chart.js</a>,
-                    <a target="_blank" href="https://whichbrowser.net/">WhichBrowser</a>.</p>
+                    <a target="_blank" href="https://github.com/econea/nusoap/">NuSOAP</a>,
+                    <a target="_blank" href="http://code.google.com/p/php-excel-reader/">Excel Reader</a>,
+                    <a target="_blank" href="http://github.com/elidickinson/php-export-data/">Export Data</a>,
+                    <a target="_blank" href="https://harvesthq.github.io/chosen/">Chosen</a>,
+                    <a target="_blank" href="https://github.com/jackocnr/intl-tel-input/">International Telephone Input</a>,
+                    <a target="_blank" href="https://github.com/qwertypants/jQuery-Word-and-Character-Counter-Plugin/">jQuery Word and character counter</a>,
+                    <a target="_blank" href="https://craftpip.github.io/jquery-confirm/">jQuery Confirm</a>.</p>
             </div>
         </div>
 
