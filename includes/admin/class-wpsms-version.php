62f38e8d210ae2d3677f1c09566749232df82408
Ali Fallah <ali.fallah.rn@gmail.com>
2020-02-01 01:23:13 +0330

    Added net.gr gateway to pro gateways

--- includes/admin/class-wpsms-version.php
+++ includes/admin/class-wpsms-version.php
@@ -200,6 +200,7 @@ class Version {
             'telnyx'		   => 'telnyx.com',
             'makolar'		   => 'makolar.com',
             'mimsms'		   => 'mimsms.com',
+			'smsnetgr'         => 'sms.net.gr',
 		);
 
 		return $gateways;
