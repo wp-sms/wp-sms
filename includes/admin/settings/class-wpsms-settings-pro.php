72c4ac55f2e1162240fceeaea05e6671d0871e32
Ali Fallah <ali.fallah.rn@gmail.com>
2020-03-14 22:32:59 +0330

    Add: added Woocommerce Notification by order Status

--- includes/admin/settings/class-wpsms-settings-pro.php
+++ includes/admin/settings/class-wpsms-settings-pro.php
@@ -607,6 +607,17 @@ class Settings_Pro {
 						          '<code>%customer_last_name%</code>'
 					          )
 				),
+				'wc_notify_by_status'           => array(
+					'id'   => 'wc_notify_by_status',
+					'name' => __( 'Notify by status', 'wp-sms' ),
+					'type' => 'header'
+				),
+				'wc_notify_by_status_content'    => array(
+					'id'      => 'wc_notify_by_status_content',
+					'name'    => __( 'Order Status & Message', 'wp-sms' ),
+					'type'    => 'repeater',
+					'desc'    => __( 'Add Order Status & Write Message Body Per Order Status', 'wp-sms' )
+				),
 			);
 		} else {
 			$wc_settings = array(
@@ -1396,6 +1407,93 @@ class Settings_Pro {
 		$html .= '<p class="description"> ' . $args['desc'] . '</p>';
 
 		echo $html;
+	}
+
+	public function repeater_callback($args)
+	{
+		if ( isset( $this->options[ $args['id'] ] ) ) {
+			$value = $this->options[ $args['id'] ];
+		} else {
+			$value = isset( $args['std'] ) ? $args['std'] : '';
+		}
+		$order_statuses = wc_get_order_statuses();
+		ob_start();
+		?>
+		<div class="repeater">
+			<div data-repeater-list="wps_pp_settings[<?php echo $args['id'] ?>]">
+				<?php if(is_array($value) && count($value)){ ?>
+					<?php foreach($value as $data){ ?>
+						<?php $order_status = isset($data['order_status']) ? $data['order_status'] : '' ?>
+						<?php $notify_status = isset($data['notify_status']) ? $data['notify_status'] : '' ?>
+						<?php $message = isset($data['message']) ? $data['message'] : '' ?>
+						<div class="repeater-item" data-repeater-item>
+							<div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc;">
+								<div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
+									<select name="order_status" style="display: block; width: 100%;">
+										<option value="">- Please Choose -</option>
+										<?php foreach ($order_statuses as $status_key => $status_name) { ?>
+											<?php $key = str_replace('wc-', '', $status_key) ?>
+											<option value="<?= $key ?>" <?= ($order_status == $key) ? 'selected' : '' ?>><?= $status_name ?></option>
+										<?php } ?>
+									</select>
+									<p class="description">Please choose an order status</p>
+								</div>
+								<div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
+									<select name="notify_status" style="display: block; width: 100%;">
+										<option value="">- Please Choose -</option>
+										<option value="1" <?= ($notify_status == '1') ? 'selected' : '' ?>>Enable</option>
+										<option value="2" <?= ($notify_status == '2') ? 'selected' : '' ?>>Disable</option>
+									</select>
+									<p class="description">Please select notify status</p>
+								</div>
+								<div style="display: block; width: 100%; margin-bottom: 15px;">
+									<textarea name="message" rows="3" style="display: block; width: 100%;"><?php echo $message ?></textarea>
+									<p class="description">Enter the contents of the SMS message.</p>
+								</div>
+								<div>
+									<input type="button" value="Delete" class="button" style="margin-bottom: 15px;" data-repeater-delete />
+								</div>
+							</div>
+						</div>
+					<?php } ?>
+				<?php } else { ?>
+					<div class="repeater-item" data-repeater-item>
+						<div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc;">
+							<div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
+								<select name="order_status" style="display: block; width: 100%;">
+									<option value="">- Please Choose -</option>
+									<?php foreach ($order_statuses as $status_key => $status_name) { ?>
+										<?php $key = str_replace('wc-', '', $status_key) ?>
+										<option value="<?= $key ?>"><?= $status_name ?></option>
+									<?php } ?>
+								</select>
+								<p class="description">Please choose an order status</p>
+							</div>
+							<div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
+								<select name="notify_status" style="display: block; width: 100%;">
+									<option value="">- Please Choose -</option>
+									<option value="1">Enable</option>
+									<option value="2">Disable</option>
+								</select>
+								<p class="description">Please select notify status</p>
+							</div>
+							<div style="display: block; width: 100%; margin-bottom: 15px;">
+								<textarea name="message" rows="3" style="display: block; width: 100%;"></textarea>
+								<p class="description">Enter the contents of the SMS message.</p>
+							</div>
+							<div>
+								<input type="button" value="Delete" class="button" style="margin-bottom: 15px;" data-repeater-delete />
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
+			<div style="margin: 10px 0;">
+				<input type="button" value="Add another order status" class="button button-primary" data-repeater-create />
+			</p>
+		</div>
+		<?php
+		echo ob_get_clean();
 	}
 
 	public function render_settings() {
