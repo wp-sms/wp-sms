e6e9ef4396caefe8910828dd15cc44150515f21f
MHitMaN <ghasemi71ir@gmail.com>
2020-09-10 22:07:16 +0430

    PRO: Add vfirst.com gateway #125

--- includes/class-wpsms-gateway.php
+++ includes/class-wpsms-gateway.php
@@ -80,6 +80,7 @@ class Gateway {
 			'makolar'          => 'makolar.com',
 			'mimsms'           => 'mimsms.com',
 			'smsto'            => 'sms.to',
+			'vfirst'            => 'vfirst.com',
 		),
 		'united states' => array(
 			'telnyx' => 'telnyx.com',
