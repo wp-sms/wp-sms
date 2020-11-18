f5c82cde31508d6b5388312ccc7b68f8fc8a9183
MHitMaN <ghasemi71ir@gmail.com>
2020-11-18 23:02:26 +0330

    Improvement: The response of the gateway now become more friendly.

--- includes/gateways/class-wpsms-gateway-reachinteractive.php
+++ includes/gateways/class-wpsms-gateway-reachinteractive.php
@@ -2,192 +2,187 @@
 
 namespace WP_SMS\Gateway;
 
-class reachinteractive extends \WP_SMS\Gateway
-{
-    private $wsdl_link = "http://http-1-uat.reach-interactive.com/sms";
-    public $tariff = "https://reach-interactive.com/";
-    public $unitrial = false;
-    public $unit;
-    public $flash = "disable";
-    public $isflash = false;
-    private $totalPerSecond = 50;
-    private $waitingSeconds = 1;
+class reachinteractive extends \WP_SMS\Gateway {
+	private $wsdl_link = "http://http-1-uat.reach-interactive.com/sms";
+	public $tariff = "https://reach-interactive.com/";
+	public $unitrial = false;
+	public $unit;
+	public $flash = "disable";
+	public $isflash = false;
+	private $totalPerSecond = 50;
+	private $waitingSeconds = 1;
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->validateNumber = "The phone number(s) the message should be sent to (must be in international format, like 447xxxxxxxxx).";
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->validateNumber = "The phone number(s) the message should be sent to (must be in international format, like 447xxxxxxxxx).";
+	}
 
-    public function SendSMS()
-    {
-        /**
-         * Modify sender number
-         *
-         * @param string $this ->from sender number.
-         *
-         * @since 3.4
-         *
-         */
-        $this->from = apply_filters('wp_sms_from', $this->from);
+	public function SendSMS() {
+		/**
+		 * Modify sender number
+		 *
+		 * @param string $this ->from sender number.
+		 *
+		 * @since 3.4
+		 *
+		 */
+		$this->from = apply_filters( 'wp_sms_from', $this->from );
 
-        /**
-         * Modify Receiver number
-         *
-         * @param array $this ->to receiver number
-         *
-         * @since 3.4
-         *
-         */
-        $this->to = apply_filters('wp_sms_to', $this->to);
+		/**
+		 * Modify Receiver number
+		 *
+		 * @param array $this ->to receiver number
+		 *
+		 * @since 3.4
+		 *
+		 */
+		$this->to = apply_filters( 'wp_sms_to', $this->to );
 
-        /**
-         * Modify text message
-         *
-         * @param string $this ->msg text message.
-         *
-         * @since 3.4
-         *
-         */
-        $this->msg = apply_filters('wp_sms_msg', $this->msg);
+		/**
+		 * Modify text message
+		 *
+		 * @param string $this ->msg text message.
+		 *
+		 * @since 3.4
+		 *
+		 */
+		$this->msg = apply_filters( 'wp_sms_msg', $this->msg );
 
-        // Get the credit.
-        $credit = $this->GetCredit();
+		// Get the credit.
+		$credit = $this->GetCredit();
 
-        // Check gateway credit
-        if (is_wp_error($credit)) {
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $credit->get_error_message(), 'error');
+		// Check gateway credit
+		if ( is_wp_error( $credit ) ) {
+			// Log the result
+			$this->log( $this->from, $this->msg, $this->to, $credit->get_error_message(), 'error' );
 
-            return $credit;
-        }
+			return $credit;
+		}
 
-        $separatedNumbers = array_chunk($this->to, $this->totalPerSecond);
-        $response         = [];
+		$separatedNumbers = array_chunk( $this->to, $this->totalPerSecond );
+		$response         = [];
 
-        foreach ($separatedNumbers as $numbers) {
-            $response = $this->send($numbers);
-        }
+		foreach ( $separatedNumbers as $numbers ) {
+			$response = $this->send( $numbers );
+		}
 
-        if (is_wp_error($response)) {
-            $this->log($this->from, $this->msg, $this->to, $response, 'error');
-            return $response;
-        }
+		if ( is_wp_error( $response ) ) {
+			$this->log( $this->from, $this->msg, $this->to, $response, 'error' );
 
-        /**
-         * Run hook after send sms.
-         *
-         * @param string $response result output.
-         *
-         * @since 2.4
-         *
-         */
-        do_action('wp_sms_send', $response);
+			return $response;
+		}
 
-        return $response;
-    }
+		/**
+		 * Run hook after send sms.
+		 *
+		 * @param string $response result output.
+		 *
+		 * @since 2.4
+		 *
+		 */
+		do_action( 'wp_sms_send', $response );
 
-    /**
-     * send SMS private method
-     *
-     * @param $numbers
-     *
-     * @return array|\WP_Error
-     */
-    private function send($numbers)
-    {
-        $encoding = 1;
-        if (isset($this->options['send_unicode']) and $this->options['send_unicode']) {
-            $encoding = 2;
-        }
+		return $response;
+	}
 
-        $args = array(
-            'headers' => array(
-                'Content-Type' => 'application/json',
-                'username'     => $this->username,
-                'password'     => $this->password,
-            ),
-            'body'    => json_encode(array(
-                'to'      => implode(',', $numbers),
-                'from'    => $this->from,
-                'message' => $this->msg,
-                'coding'  => $encoding,
-            ))
-        );
+	/**
+	 * send SMS private method
+	 *
+	 * @param $numbers
+	 *
+	 * @return array|\WP_Error
+	 */
+	private function send( $numbers ) {
+		$encoding = 1;
+		if ( isset( $this->options['send_unicode'] ) and $this->options['send_unicode'] ) {
+			$encoding = 2;
+		}
 
-        // Time to wait
-        sleep($this->waitingSeconds);
+		$args = array(
+			'headers' => array(
+				'Content-Type' => 'application/json',
+				'username'     => $this->username,
+				'password'     => $this->password,
+			),
+			'body'    => json_encode( array(
+				'to'      => implode( ',', $numbers ),
+				'from'    => $this->from,
+				'message' => $this->msg,
+				'coding'  => $encoding,
+			) )
+		);
 
-        $response = wp_remote_post($this->wsdl_link . "/message", $args);
+		// Time to wait
+		sleep( $this->waitingSeconds );
 
-        return $this->handleResponse($response, $numbers);
-    }
+		$response = wp_remote_post( $this->wsdl_link . "/message", $args );
 
-    /**
-     * Check response
-     *
-     * @param $response
-     *
-     * @param $numbers
-     *
-     * @return array|\WP_Error
-     */
-    private function handleResponse($response, $numbers)
-    {
-        if (is_wp_error($response)) {
-            return $response;
-        }
+		return $this->handleResponse( $response, $numbers );
+	}
 
-        if (wp_remote_retrieve_response_code($response) != 200) {
-            return new \WP_Error('send-sms', $response);
-        }
+	/**
+	 * Check response
+	 *
+	 * @param $response
+	 *
+	 * @param $numbers
+	 *
+	 * @return array|\WP_Error
+	 */
+	private function handleResponse( $response, $numbers ) {
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
 
-        $response = json_decode($response['body']);
+		if ( wp_remote_retrieve_response_code( $response ) != 200 ) {
+			return new \WP_Error( 'send-sms', __( 'For more information, please check the full response on Outbox.', 'wp-sms-pro' ) );
+		}
 
-        foreach ($response as $item) {
-            if (!$item->Success or $item->Success == '') {
-                return new \WP_Error('send-sms', $item->Description);
-            }
-        }
+		$response = json_decode( $response['body'] );
 
-        // Log the result
-        $this->log($this->from, $this->msg, $numbers, $response);
+		foreach ( $response as $item ) {
+			if ( ! $item->Success or $item->Success == '' ) {
+				return new \WP_Error( 'send-sms', $item->Description );
+			}
+		}
 
-        return $response;
-    }
+		// Log the result
+		$this->log( $this->from, $this->msg, $numbers, $response );
 
-    public function GetCredit()
-    {
-        // Check username and password
-        if (!$this->username or !$this->password) {
-            return new \WP_Error('account-credit', __('Username/Password does not set for this gateway', 'wp-sms-pro'));
-        }
+		return $response;
+	}
 
-        $args = array(
-            'headers' => array(
-                'Content-Type' => 'application/json',
-                'username'     => $this->username,
-                'password'     => $this->password,
-            )
-        );
+	public function GetCredit() {
+		// Check username and password
+		if ( ! $this->username or ! $this->password ) {
+			return new \WP_Error( 'account-credit', __( 'Username/Password does not set for this gateway', 'wp-sms-pro' ) );
+		}
 
-        $response = wp_remote_get($this->wsdl_link . "/balance", $args);
+		$args = array(
+			'headers' => array(
+				'Content-Type' => 'application/json',
+				'username'     => $this->username,
+				'password'     => $this->password,
+			)
+		);
 
-        // Check response
-        if (is_wp_error($response)) {
-            return new \WP_Error('send-sms', $response->get_error_message());
-        }
+		$response = wp_remote_get( $this->wsdl_link . "/balance", $args );
 
-        $result = json_decode($response['body'], true);
+		// Check response
+		if ( is_wp_error( $response ) ) {
+			return new \WP_Error( 'send-sms', $response->get_error_message() );
+		}
 
-        if ($result['Success'] == 1) {
-            if ($result['Balance'] == 0) {
-                return new \WP_Error('account-credit', sprintf('%s: %s', $result['Description'], $result['Balance']));
-            }
+		$result = json_decode( $response['body'], true );
 
-            return $result['Balance'];
-        }
+		if ( $result['Success'] == 1 ) {
+			if ( $result['Balance'] == 0 ) {
+				return new \WP_Error( 'account-credit', sprintf( '%s: %s', $result['Description'], $result['Balance'] ) );
+			}
 
-        return new \WP_Error('account-credit', $result['Description']);
-    }
+			return $result['Balance'];
+		}
+
+		return new \WP_Error( 'account-credit', $result['Description'] );
+	}
 }
