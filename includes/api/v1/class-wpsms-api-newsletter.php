62ff84a5afad683b4130c886e40e0d220f390e5c
Mostafa Soufi <mst404@gmail.com>
2021-03-31 22:16:54 +0300

    Fix SMS newsletter verification

--- includes/api/v1/class-wpsms-api-newsletter.php
+++ includes/api/v1/class-wpsms-api-newsletter.php
@@ -131,7 +131,7 @@ class Newsletter extends \WP_SMS\RestApi
         $params = $request->get_params();
         $number = self::convertNumber($params['mobile']);
 
-        $group_id = isset ($params['group_id']) ? $params['group_id'] : 1;
+        $group_id = isset ($params['group_id']) ? $params['group_id'] : 0;
         $result   = self::verifySubscriber($params['name'], $number, $params['activation'], $group_id);
 
         if (is_wp_error($result)) {
