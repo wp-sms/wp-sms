9901cf90c6f952a8194d2474ba20f9c40a1e4d2b
Mostafa Soufi <mst404@gmail.com>
2021-06-07 21:11:10 +0300

    Update dexatel api version

--- includes/gateways/class-wpsms-gateway-dexatel.php
+++ includes/gateways/class-wpsms-gateway-dexatel.php
@@ -4,17 +4,19 @@ namespace WP_SMS\Gateway;
 
 class dexatel extends \WP_SMS\Gateway
 {
-    private $wsdl_link = "https://sms.dexatel.com:8002/api";
+    private $wsdl_link = "https://api.dexatel.com/v1";
     public $tariff = "https://dexatel.com";
     public $unitrial = true;
     public $unit;
     public $flash = "disable";
     public $isflash = false;
+    public $has_key = true;
 
     public function __construct()
     {
         parent::__construct();
         $this->validateNumber = 'Must be sent in international E.164 format (up to 15 digits allowed)';
+        $this->help           = 'Please fill out your TOKEN in the API key field.';
     }
 
     public function SendSMS()
@@ -52,47 +54,33 @@ class dexatel extends \WP_SMS\Gateway
         // Check gateway credit
         if (is_wp_error($credit)) {
             $this->log($this->from, $this->msg, $this->to, $credit->get_error_message(), 'error');
-            return $credit;
+            //return $credit;
         }
 
-        $request = add_query_arg([
-            'username' => $this->username,
-            'password' => $this->password,
-            'dnis'     => implode(',', $this->to),
-            'ani'      => $this->from,
-            'message'  => urlencode($this->msg),
-            'command'  => 'submit',
-        ], $this->wsdl_link);
+        $args = [
+            'headers' => [
+                'token'        => $this->has_key,
+                'Content-Type' => 'application/json'
+            ],
+            'body'    => json_encode([
+                'messages' => [
+                    'phone'       => $this->to,
+                    'sender_name' => $this->from,
+                    'message'     => $this->msg
+                ]
+            ])
+        ];
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL            => $request,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING       => '',
-            CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_TIMEOUT        => 5,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST  => 'GET',
-        ));
-
-        $httpCode     = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        $response     = curl_exec($curl);
-        $errorMessage = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($httpCode == 0 or $httpCode != 200) {
-            $response = $response ?: $errorMessage;
-            $this->log($this->from, $this->msg, $this->to, $response, 'error');
-
-            return new \WP_Error('send-sms', $response);
+        $response      = wp_remote_post($this->wsdl_link . '/send/sms/bulk', $args);
+        $response_code = wp_remote_retrieve_response_code($response);
+        $response      = json_decode($response['body']);
+        
+        if ($response_code != '200') {
+            return new \WP_Error('send-sms', $response->message);
         }
-
-        $responseBody = json_decode($response);
 
         // Log the result
-        $this->log($this->from, $this->msg, $this->to, $responseBody);
+        $this->log($this->from, $this->msg, $this->to, $response);
 
         /**
          * Run hook after send sms.
@@ -101,18 +89,37 @@ class dexatel extends \WP_SMS\Gateway
          * @since 2.4
          *
          */
-        do_action('wp_sms_send', $responseBody);
+        do_action('wp_sms_send', $response);
 
-        return $responseBody;
+        return $response;
     }
 
     public function GetCredit()
     {
         // Check username and password
-        if (!$this->username && !$this->password) {
-            return new \WP_Error('account-credit', __('Username/Password is required.', 'wp-sms'));
+        if (!$this->username or !$this->password) {
+            return new \WP_Error('account-credit', __('API username or API password is not entered.', 'wp-sms-pro'));
         }
 
-        return 1;
+        $response = wp_remote_get($this->wsdl_link . '/get_balance', [
+            'headers' => [
+                'token'        => $this->has_key,
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+
+        // Check gateway credit
+        if (is_wp_error($response)) {
+            return new \WP_Error('account-credit', $response->get_error_message());
+        }
+
+        $response_code = wp_remote_retrieve_response_code($response);
+        $response      = json_decode($response['body']);
+
+        if ($response_code != '200') {
+            return new \WP_Error('account-credit', $response->message);
+        }
+
+        return $response->data->balance;
     }
 }
