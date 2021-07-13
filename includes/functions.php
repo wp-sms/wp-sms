e96451adfe7a36ba940c3d136564594fa3a980db
Mostafa Soufi <mst404@gmail.com>
2021-07-14 00:06:05 +0300

    Fix wrong sanitize data field

--- includes/functions.php
+++ includes/functions.php
@@ -15,3 +15,24 @@ function wp_sms_initial_gateway()
 
     return Gateway::initial();
 }
+
+/**
+ * @param $array_or_string
+ * @return mixed|string
+ */
+function wp_sms_sanitize_array($array_or_string)
+{
+    if (is_string($array_or_string)) {
+        $array_or_string = sanitize_text_field($array_or_string);
+    } elseif (is_array($array_or_string)) {
+        foreach ($array_or_string as $key => &$value) {
+            if (is_array($value)) {
+                $value = sanitize_text_or_array_field($value);
+            } else {
+                $value = sanitize_text_field($value);
+            }
+        }
+    }
+
+    return $array_or_string;
+}
--- includes/templates/wpcf7-form.php
+++ includes/templates/wpcf7-form.php
@@ -7,7 +7,7 @@
             <tr>
                 <th scope="row"><label for="wpcf7-sms-sender"><?php _e('Send to', 'wp-sms'); ?>:</label></th>
                 <td>
-                    <input type="text" value="<?php echo $cf7_options['phone']; ?>" size="70" class="large-text code" name="wpcf7-sms[phone]" id="wpcf7-sms-sender">
+                    <input type="text" value="<?php echo $cf7_options['phone'] ?? ''; ?>" size="70" class="large-text code" name="wpcf7-sms[phone]" id="wpcf7-sms-sender">
                     <p class="description"><?php _e('<b>Note:</b> To send more than one number, separate the numbers with a comma. (e.g. 10000000001,10000000002)', 'wp-sms'); ?></p>
                 </td>
             </tr>
@@ -15,7 +15,7 @@
             <tr>
                 <th scope="row"><label for="wpcf7-sms-message"><?php _e('Message body', 'wp-sms'); ?>:</label></th>
                 <td>
-                    <textarea class="large-text" rows="4" cols="100" name="wpcf7-sms[message]" id="wpcf7-sms-message"><?php echo $cf7_options['message']; ?></textarea>
+                    <textarea class="large-text" rows="4" cols="100" name="wpcf7-sms[message]" id="wpcf7-sms-message"><?php echo $cf7_options['message'] ?? ''; ?></textarea>
                     <p class="description"><?php _e('<b>Note:</b> Use %% Instead of [], for example: %your-name%', 'wp-sms'); ?></p>
                 </td>
             </tr>
@@ -28,7 +28,7 @@
                 <th scope="row"><label for="wpcf7-sms-sender-form"><?php _e('Send to field', 'wp-sms'); ?>:</label>
                 </th>
                 <td>
-                    <input type="text" value="<?php echo $cf7_options_field['phone']; ?>" size="70" class="large-text code" name="wpcf7-sms-form[phone]" id="wpcf7-sms-sender-form">
+                    <input type="text" value="<?php echo $cf7_options_field['phone'] ?? ''; ?>" size="70" class="large-text code" name="wpcf7-sms-form[phone]" id="wpcf7-sms-sender-form">
                     <p class="description"><?php _e('<b>Note:</b> Use %% Instead of [], for example: %your-mobile%', 'wp-sms'); ?></p>
                 </td>
             </tr>
@@ -37,7 +37,7 @@
                 <th scope="row"><label for="wpcf7-sms-message-form"><?php _e('Message body', 'wp-sms'); ?>:</label>
                 </th>
                 <td>
-                    <textarea class="large-text" rows="4" cols="100" name="wpcf7-sms-form[message]" id="wpcf7-sms-message-form"><?php echo $cf7_options_field['message']; ?></textarea>
+                    <textarea class="large-text" rows="4" cols="100" name="wpcf7-sms-form[message]" id="wpcf7-sms-message-form"><?php echo $cf7_options_field['message'] ?? ''; ?></textarea>
                     <p class="description"><?php _e('<b>Note:</b> Use %% Instead of [], for example: %your-name%', 'wp-sms'); ?></p>
                 </td>
             </tr>
