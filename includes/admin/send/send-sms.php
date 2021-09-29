b2558b39c2d909a22d56e2aeeec0d3baec2daefe
Mostafa Soufi <mst404@gmail.com>
2021-09-29 22:54:03 +0300

    Remove unused jQuery code

--- includes/admin/send/send-sms.php
+++ includes/admin/send/send-sms.php
@@ -53,23 +53,6 @@
             }
         });
         <?php endif; ?>
-
-        jQuery(document).ready(function ($) {
-
-            jQuery('#upload_image_button').click(function () {
-
-                formfield = jQuery('#upload_image').attr('name');
-                tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
-                window.send_to_editor = function (html) {
-                    imgurl = jQuery(html).attr('src');
-                    jQuery('#upload_image').val(imgurl);
-                    tb_remove();
-                }
-
-                return false;
-            });
-
-        });
     });
 </script>
 <div class="wrap wpsms-wrap">
