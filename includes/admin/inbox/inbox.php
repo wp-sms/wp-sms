7ec91b9a22ed0547b0ee81708a519bf46a8a1536
farshid  rahimi <fo4rty@gmail.com>
2021-11-22 12:50:48 +0330

    Implement the dot :)

--- includes/admin/inbox/inbox.php
+++ includes/admin/inbox/inbox.php
@@ -3,7 +3,7 @@
     <div class="wpsms-wrap__main wpsms-inbox-page">
         <img class="background-img" src="<?= WP_SMS_URL . '/assets/images/blurred-inbox.png' ?>" alt="">
         <div class="promotion-modal">
-            <?= __('This feature is available through the WP-SMS Two-Way add-on which allows you to receive messages and do actions with them such as unsubscribe, cancel order, etc ..<br> to get more information and enable this feature click <a target="_blank" href="' . WP_SMS_SITE . '/product/wp-sms-two-way/">here</a>'); ?>
+            <?= __('This feature is available through the WP-SMS Two-Way add-on which allows you to receive messages and do actions with them such as unsubscribe, cancel order, etc ...<br> to get more information and enable this feature click <a target="_blank" href="' . WP_SMS_SITE . '/product/wp-sms-two-way/">here</a>'); ?>
         </div>
     </div>
 </div>
