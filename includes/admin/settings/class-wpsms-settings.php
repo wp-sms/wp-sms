b360cc436057e35feb6188544638fec5d0f9cdfc
Mostafa Soufi <mst404@gmail.com>
2021-11-28 16:00:31 +0200

    Add Add-Ons page

--- includes/admin/settings/class-wpsms-settings.php
+++ includes/admin/settings/class-wpsms-settings.php
@@ -44,7 +44,7 @@ class Settings
         add_submenu_page('wp-sms', __('Settings', 'wp-sms'), __('Settings', 'wp-sms'), 'wpsms_setting', 'wp-sms-settings', array(
             $this,
             'render_settings'
-        ));
+        ), 6);
     }
 
     /**
