6208cb2450e4512bbf5b015de2053829db757c05
Mostafa Soufi <mst404@gmail.com>
2021-12-08 18:26:14 +0200

    Update WooCommerce setting page

--- includes/admin/settings/class-wpsms-settings.php
+++ includes/admin/settings/class-wpsms-settings.php
@@ -639,7 +639,7 @@ class Settings
                     'type' => 'textarea',
                     'desc' => __('Enter the contents of the SMS message.', 'wp-sms') . '<br>' .
                         sprintf(
-                            __('Billing First Name: %s, Billing Company: %s, Billing Address: %s, Billing Phone Number: %s, Order ID: %s, Order number: %s, Order Total: %s, Order edit URL: %s, Order status: %s', 'wp-sms'),
+                            __('Billing First Name: %s, Billing Company: %s, Billing Address: %s, Billing Phone Number: %s, Order ID: %s, Order number: %s, Order Total: %s, Order edit URL: %s, Order Items: %s, Order status: %s', 'wp-sms'),
                             '<code>%billing_first_name%</code>',
                             '<code>%billing_company%</code>',
                             '<code>%billing_address%</code>',
@@ -648,6 +648,7 @@ class Settings
                             '<code>%order_number%</code>',
                             '<code>%order_total%</code>',
                             '<code>%order_edit_url%</code>',
+                            '<code>%order_items%</code>',
                             '<code>%status%</code>'
                         )
                 ),
@@ -669,10 +670,11 @@ class Settings
                     'type' => 'textarea',
                     'desc' => __('Enter the contents of the SMS message.', 'wp-sms') . '<br>' .
                         sprintf(
-                            __('Order ID: %s, Order number: %s, Order status: %s, Order Total: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'),
+                            __('Order ID: %s, Order number: %s, Order status: %s, Order Items: %s, Order Total: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'),
                             '<code>%order_id%</code>',
                             '<code>%order_number%</code>',
                             '<code>%status%</code>',
+                            '<code>%order_items%</code>',
                             '<code>%order_total%</code>',
                             '<code>%billing_first_name%</code>',
                             '<code>%billing_last_name%</code>',
@@ -2001,7 +2003,7 @@ class Settings
 								<div style="display: block; width: 100%; margin-bottom: 15px;">
 									<textarea name="message" rows="3" style="display: block; width: 100%;"><?php echo $message ?></textarea>
 									<p class="description">Enter the contents of the SMS message.</p>
-									<p class="description"><?php echo sprintf(__('Order status: %s, Order number: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'), '<code>%status%</code>', '<code>%order_number%</code>', '<code>%customer_first_name%</code>', '<code>%customer_last_name%</code>', '<code>%order_view_url%</code>', '<code>%order_pay_url%</code>') ?></p>
+									<p class="description"><?php echo sprintf(__('Order status: %s, Order Items: %s, Order number: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'), '<code>%status%</code>', '<code>%order_items%</code>', '<code>%order_number%</code>', '<code>%customer_first_name%</code>', '<code>%customer_last_name%</code>', '<code>%order_view_url%</code>', '<code>%order_pay_url%</code>') ?></p>
 								</div>
 								<div>
 									<input type="button" value="Delete" class="button" style="margin-bottom: 15px;" data-repeater-delete />
@@ -2033,7 +2035,7 @@ class Settings
 							<div style="display: block; width: 100%; margin-bottom: 15px;">
 								<textarea name="message" rows="3" style="display: block; width: 100%;"></textarea>
 								<p class="description">Enter the contents of the SMS message.</p>
-								<p class="description"><?php echo sprintf(__('Order status: %s, Order number: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'), '<code>%status%</code>', '<code>%order_number%</code>', '<code>%customer_first_name%</code>', '<code>%customer_last_name%</code>', '<code>%order_view_url%</code>', '<code>%order_pay_url%</code>') ?></p>
+								<p class="description"><?php echo sprintf(__('Order status: %s, Order Items: %s, Order number: %s, Customer name: %s, Customer family: %s, Order view URL: %s, Order payment URL: %s', 'wp-sms'), '<code>%status%</code>', '<code>%order_items%</code>', '<code>%order_number%</code>', '<code>%customer_first_name%</code>', '<code>%customer_last_name%</code>', '<code>%order_view_url%</code>', '<code>%order_pay_url%</code>') ?></p>
 							</div>
 							<div>
 								<input type="button" value="Delete" class="button" style="margin-bottom: 15px;" data-repeater-delete />
