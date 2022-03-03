72d389fcbde422f23f5b4f48c164b6d7fa272cc3
farshid  rahimi <fo4rty@gmail.com>
2022-03-03 17:28:59 +0330

    Remove class finder from helpers

--- includes/Helper.php
+++ includes/Helper.php
@@ -52,46 +52,4 @@ class Helper
 
         return apply_filters('wp_sms_user_mobile_number', $mobileNumber, $userId);
     }
-
-    /**
-     * Get all Fully Qualified Class Names existing in a directory
-     *
-     * @see https://stackoverflow.com/a/27440555
-     * @param string $path
-     * @return array
-     */
-    public static function findAllClassesInDir(string $path): array
-    {
-        $fqcns = [];
-
-        if (!is_dir($path)) {
-            return $fqcns;
-        }
-
-        $allFiles = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
-        $phpFiles = new \RegexIterator($allFiles, '/\.php$/');
-        foreach ($phpFiles as $phpFile) {
-            $path = $phpFile->getRealPath();
-            $content = file_get_contents($path);
-            $tokens = token_get_all($content);
-            $namespace = '';
-            for ($index = 0; isset($tokens[$index]); $index++) {
-                if (!isset($tokens[$index][0])) {
-                    continue;
-                }
-                if (T_NAMESPACE === $tokens[$index][0]) {
-                    $index += 2; // Skip namespace keyword and whitespace
-                    while (isset($tokens[$index]) && is_array($tokens[$index])) {
-                        $namespace .= $tokens[$index++][1];
-                    }
-                }
-                if (T_CLASS === $tokens[$index][0] && T_WHITESPACE === $tokens[$index + 1][0] && T_STRING === $tokens[$index + 2][0]) {
-                    $index += 2; // Skip class keyword and whitespace
-                    $fqcns[$path] = $namespace.'\\'.$tokens[$index][1];
-                    break;
-                }
-            }
-        }
-        return $fqcns;
-    }
 }
