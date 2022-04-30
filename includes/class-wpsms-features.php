afd6393e6f6f7638d33bfcf17ca90a06b7f8fb73
Mostafa Soufi <mst404@gmail.com>
2022-04-30 17:56:43 +0300

    Possibility to select the mobile country code instead of entering them

--- includes/class-wpsms-features.php
+++ includes/class-wpsms-features.php
@@ -207,8 +207,8 @@ class Features
     {
 
         //Register IntelTelInput Assets
-        wp_enqueue_style('wpsms-intel-tel-input', WP_SMS_URL . 'assets/css/intlTelInput.min.css', true, WP_SMS_VERSION);
-        wp_enqueue_script('wpsms-intel-tel-input', WP_SMS_URL . 'assets/js/intel/intlTelInput.min.js', array('jquery'), WP_SMS_VERSION, true);
+        wp_enqueue_style('wpsms-intel-tel-input', WP_SMS_URL . 'assets/css/intlTelInput.min.css', true, '17.0.0');
+        wp_enqueue_script('wpsms-intel-tel-input', WP_SMS_URL . 'assets/js/intel/intlTelInput.min.js', array('jquery'), '17.0.0', true);
         wp_enqueue_script('wpsms-intel-script', WP_SMS_URL . 'assets/js/intel/intel-script.js', true, WP_SMS_VERSION, true);
 
         // Localize the IntelTelInput
