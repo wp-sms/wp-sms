fbaca516d6e5953dcf939e1d174fa062af5dff70
Mohammadreza Taheri <taheri.reza94@gmail.com>
2022-06-23 15:58:11 +0430

    minor changes in aobox gateway

--- includes/gateways/class-wpsms-gateway-aobox.php
+++ includes/gateways/class-wpsms-gateway-aobox.php
@@ -7,38 +7,38 @@ use WP_Error;
 
 class aobox extends \WP_SMS\Gateway
 {
-    private string $wsdl_link = " https://aobox.it/app";
-    public string $tariff = "https://www.aobox.it";
-    public bool $unitrial = false;
-    public string $unit;
-    public string $flash = "disable";
+    private $wsdl_link = " https://aobox.it/app";
+    public $tariff = "https://www.aobox.it";
+    public $unitrial = false;
+    public $unit;
+    public $flash = "disable";
     public $isflash = false;
-    public string $gateway_route;
+    public $gateway_route;
 
     public function __construct()
     {
         parent::__construct();
-        $this->bulk_send = true;
+        $this->bulk_send      = true;
         $this->validateNumber = "Recipient number(s) with international prefix without “+”. example: 393351234567,393337654321,393880000123";
-        $this->help = "Just enter your username and password.";
-        $this->gatewayFields = [
+        $this->help           = "Just enter your username and password.";
+        $this->gatewayFields  = [
             'username' => [
-                'id' => 'gateway_username',
+                'id'   => 'gateway_username',
                 'name' => 'Username',
                 'desc' => 'Enter your username.',
             ],
             'password' => [
-                'id' => 'gateway_password',
+                'id'   => 'gateway_password',
                 'name' => 'Password',
                 'desc' => 'Enter your password.',
             ],
-            'from' => [
-                'id' => 'gateway_sender_id',
+            'from'     => [
+                'id'   => 'gateway_sender_id',
                 'name' => 'Sender',
                 'desc' => 'The SMS sender, maximum 11 characters. If not set “AObox” will be used as sender. Note: some routes do not allow a free sender and some routes only allow a fixed sender. ',
             ],
-            'route' => [
-                'id' => 'gateway_route',
+            'route'    => [
+                'id'   => 'gateway_route',
                 'name' => 'Route',
                 'desc' => 'The gateway route via which the SMS is sent. Different routes have different features and quality. Route numbers you can use are supplied by your sales account.',
             ],
@@ -85,13 +85,13 @@ class aobox extends \WP_SMS\Gateway
             }
 
             $params = [
-                'version' => '3',
+                'version'  => '3',
                 'username' => $this->username,
                 'password' => $this->password,
-                'route' => $this->gateway_route ?? '3',
-                'sender' => $this->from ?? null,
-                'rcpt' => implode(',', $this->to),
-                'text' => $this->msg,
+                'route'    => $this->gateway_route ?? '3',
+                'sender'   => $this->from ?? null,
+                'rcpt'     => implode(',', $this->to),
+                'text'     => $this->msg,
             ];
 
             $response = $this->request('POST', "$this->wsdl_link/gateway.php", $params, []);
@@ -121,7 +121,7 @@ class aobox extends \WP_SMS\Gateway
         }
     }
 
-    public function GetCredit(): WP_Error|string
+    public function GetCredit()
     {
         try {
 
