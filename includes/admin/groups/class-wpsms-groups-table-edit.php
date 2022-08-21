d54c7a20cd6045c32b3577682152da94d4d664d0
Mohammadreza Taheri <taheri.reza94@gmail.com>
2022-08-21 16:01:18 +0430

    add group add/edit form as a template

--- includes/admin/groups/class-wpsms-groups-table-edit.php
+++ includes/admin/groups/class-wpsms-groups-table-edit.php
@@ -28,31 +28,13 @@ class Subscribers_Groups_Table_Edit
         //set Actiom Values
         $group_id   = isset($_GET['group_id']) ? sanitize_text_field($_GET['group_id']) : null;
         $group_name = isset($_GET['group_name']) ? sanitize_text_field($_GET['group_name']) : null;
-        $html       = '<form action="" method="post">
-					    <table>
-					        <tr>
-					            <td style="padding-top: 10px;">
-					                <label for="wp_group_name"
-					                       class="wp_sms_subscribers_label">' . __('Name', 'wp-sms') . '</label>
-					                <input type="text" id="wp_group_name" name="wp_group_name" value="' . esc_attr($group_name) . '"
-					                       class="wp_sms_subscribers_input_text"/>
-					                <input type="hidden" id="wp_group_name" name="group_id" value="' . $group_id . '"
-							class="wp_sms_subscribers_input_text"/>
-							</td>
-							</tr>
-							
-							<tr>
-							    <td colspan="2" style="padding-top: 20px;">
-							        <input type="submit" class="button-primary" name="wp_update_group"
-							               value="' . __('Edit', 'wp-sms') . '"/>
-							    </td>
-							</tr>
-							</table>
-						</form>';
-        echo $html;
-        wp_die(); // this is required to terminate immediately and return a proper response
-    }
 
+        echo Helper::loadTemplate('/admin/group-form.php', array(
+            'group_id'   => $group_id,
+            'group_name' => $group_name
+        ));
+        exit;
+    }
 }
 
 new Subscribers_Groups_Table_Edit();
