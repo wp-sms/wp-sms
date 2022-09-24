e94738f6db107c7fd286dd137564b7d6e985ea6f
Mohammadreza Taheri <taheri.reza94@gmail.com>
2022-09-24 13:03:01 +0330

    fix smsto gateway

--- includes/class-wpsms-gateway.php
+++ includes/class-wpsms-gateway.php
@@ -26,7 +26,7 @@ class Gateway
             'nexmo'          => 'nexmo.com',
             'clockworksms'   => 'clockworksms.com',
             'messagebird'    => 'messagebird.com',
-            'smstos'         => 'sms.to',
+            'smsto'         => 'sms.to',
             'clicksend'      => 'clicksend.com',
             'globalvoice'    => 'global-voice.net',
             'smsapicom'      => 'smsapi.com',
