99729354b8c00b588ec4a696ee000b86b29b6368
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2022-10-15 13:48:16 +0330

    Fix search gateways name issue

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -10,8 +10,41 @@
     let WpSmsSelect2 = $('.js-wpsms-select2')
     let WpSmsExportForm = $('.js-wpSmsExportForm')
 
+    function matchCustom(params, data) {
+        // If there are no search terms, return all of the data
+        if ($.trim(params.term) === '') {
+            return data;
+        }
+
+        // Do not display the item if there is no 'text' property
+        if (typeof data.text === 'undefined') {
+            return null;
+        }
+
+        // `params.term` should be the term that is used for searching
+        // `data.text` is the text that is displayed for the data object
+        if (data.text.indexOf(params.term) > -1 || data.element.getAttribute('value') !== null && data.element.getAttribute('value').toLowerCase().indexOf(params.term.toLowerCase()) > -1) {
+            var modifiedData = $.extend({}, data, true);
+            modifiedData.text += ' (matched)';
+            
+            // You can return modified objects from here
+            // This includes matching the `children` how you want in nested data sets
+            return modifiedData;
+        }
+
+        // Return `null` if the term should not be displayed
+        return null;
+    }
+
     const WpSmsSelect2Options = {
-        placeholder: "Please select"
+        placeholder: "Please select",
+        matcher: matchCustom,
+        sortResults: function (results) {
+            if (results.length > 1) {
+                results.pop();
+            }
+            return results;
+        },
     };
 
     if (WpSmsExportForm.length) {
