f28f03366529b8e6f63fc3784c678840826d38cd
Mohammadreza Taheri <taheri.reza94@gmail.com>
2022-11-22 18:03:10 +0330

    Hostiran gateway has been updated

--- includes/gateways/class-wpsms-gateway-hostiran.php
+++ includes/gateways/class-wpsms-gateway-hostiran.php
@@ -2,19 +2,47 @@
 
 namespace WP_SMS\Gateway;
 
+use Exception;
+use WP_Error;
+
 class hostiran extends \WP_SMS\Gateway
 {
-    private $wsdl_link = "http://sms.hostiran.net/webservice/?WSDL";
+    private $wsdl_link = "https://rest.payamak-panel.com/api/SendSMS";
     public $tariff = "http://sms.hostiran.net";
     public $unitrial = true;
     public $unit;
-    public $flash = "disable";
+    public $flash = "false";
     public $isflash = false;
 
     public function __construct()
     {
         parent::__construct();
+        $this->help           = "";
         $this->validateNumber = "09xxxxxxxx";
+        $this->has_key        = true;
+        $this->bulk_send      = true;
+        $this->gatewayFields  = [
+            'username' => [
+                'id'   => 'gateway_username',
+                'name' => 'Username',
+                'desc' => 'Enter your username',
+            ],
+            'password' => [
+                'id'   => 'gateway_password',
+                'name' => 'Password',
+                'desc' => 'Enter your password',
+            ],
+            'has_key'  => [
+                'id'   => 'gateway_key',
+                'name' => 'API Token',
+                'desc' => 'Enter your API token.',
+            ],
+            'from'     => [
+                'id'   => 'gateway_sender_id',
+                'name' => 'Sender ID',
+                'desc' => 'Enter your sender ID',
+            ],
+        ];
 
         @ini_set("soap.wsdl_cache_enabled", "0");
     }
@@ -48,64 +76,77 @@ class hostiran extends \WP_SMS\Gateway
          */
         $this->msg = apply_filters('wp_sms_msg', $this->msg);
 
-        $options = array('login' => $this->username, 'password' => $this->password);
-
-        // Get the credit.
-        $credit = $this->GetCredit();
-
-        // Check gateway credit
-        if (is_wp_error($credit)) {
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $credit->get_error_message(), 'error');
-
-            return $credit;
-        }
 
         try {
-            $client = new \SoapClient($this->wsdl_link, $options);
-            $result = $client->sendToMany($this->to, $this->msg, $this->from);
 
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $result);
+            $arguments = [
+                'header' => [
+                    'token' => $this->has_key
+                ],
+                'body'   => [
+                    'username' => $this->username,
+                    'password' => $this->password,
+                    'to'       => implode(',', $this->to),
+                    'from'     => $this->from,
+                    'text'     => $this->msg
+                ]
+            ];
+
+            $response = $this->request('POST', "{$this->wsdl_link}/SendSMS", [], $arguments);
+
+            if ($response->StrRetStatus !== 'Ok') {
+                throw new Exception($response);
+            }
+
+            //log the result
+            $this->log($this->from, $this->msg, $this->to, $response);
 
             /**
              * Run hook after send sms.
              *
-             * @param string $result result output.
+             * @param string $response result output.
              * @since 2.4
              *
              */
-            do_action('wp_sms_send', $result);
+            do_action('wp_sms_send', $response);
 
-            return $result;
-        } catch (\Exception $e) {
-            // Log th result
+            return $response;
+
+        } catch (Exception $e) {
             $this->log($this->from, $this->msg, $this->to, $e->getMessage(), 'error');
 
-            return new \WP_Error('send-sms', $e->getMessage());
+            return new WP_Error('send-sms', $e->getMessage());
         }
     }
 
     public function GetCredit()
     {
-        // Check username and password
-        if (!$this->username && !$this->password) {
-            return new \WP_Error('account-credit', __('Username and Password are required.', 'wp-sms'));
-        }
-
-        if (!class_exists('SoapClient')) {
-            return new \WP_Error('required-class', __('Class SoapClient not found. please enable php_soap in your php.', 'wp-sms'));
-        }
-
-        $options = array('login' => $this->username, 'password' => $this->password);
-
         try {
-            $client = new \SoapClient($this->wsdl_link, $options);
-            $credit = $client->accountInfo();
+            // Check username and password
+            if (!$this->username && !$this->password) {
+                return new \WP_Error('account-credit', __('Username and Password are required.', 'wp-sms'));
+            }
 
-            return $credit->remaining;
-        } catch (\Exception $e) {
-            return new \WP_Error('account-credit', $e->getMessage());
+            $arguments = [
+                'header' => [
+                    'token' => $this->has_key
+                ],
+                'body'   => [
+                    'username' => $this->username,
+                    'password' => $this->password
+                ]
+            ];
+
+            $response = $this->request('POST', "{$this->wsdl_link}/GetCredit", [], $arguments);
+
+            if ($response->StrRetStatus !== 'Ok') {
+                throw new Exception($response);
+            }
+
+            return $response;
+
+        } catch (Exception $e) {
+            return new WP_Error('account-credit', $e->getMessage());
         }
     }
 }
