e3100e607df5896e5d5e1efdf45b0c11630f60f4
Mostafa Soufi <mostafa@veronalabs.com>
2023-02-10 00:07:39 +0330

    Finish-up WooCommerceAddMobileFieldHandler

--- src/User/MobileFieldHandler/WooCommerceAddMobileFieldHandler.php
+++ src/User/MobileFieldHandler/WooCommerceAddMobileFieldHandler.php
@@ -2,10 +2,71 @@
 
 namespace WP_SMS\User\MobileFieldHandler;
 
+use WP_SMS\Helper;
+
 class WooCommerceAddMobileFieldHandler
 {
+    /**
+     * @var mixed|null
+     */
+    private $mobileField;
+
+    public function __construct()
+    {
+        $this->mobileField = \WP_SMS\Helper::getUserMobileFieldName();
+    }
+
     public function register()
     {
-        // todo add_action
+        // billing address
+        add_filter('woocommerce_billing_fields', [$this, 'registerFieldInBillingForm']);
+        add_action('woocommerce_save_account_details_errors', [$this, 'validateMobileNumberCallback']);
+
+        // checkout billing address
+        add_filter('woocommerce_checkout_fields', [$this, 'registerFieldInCheckoutBillingForm']);
+        add_action('woocommerce_after_checkout_validation', [$this, 'validateMobileNumberCallback'], 10, 2);
+
+    }
+
+    public function registerFieldInBillingForm($fields)
+    {
+        $fields[$this->mobileField] = $this->getField();
+
+        return $fields;
+    }
+
+    /**
+     * @param $errors \WP_Error
+     * @return void
+     */
+    public function validateMobileNumberCallback($errors)
+    {
+        $mobile   = Helper::sanitizeMobileNumber($_POST[$this->mobileField]);
+        $validity = Helper::checkMobileNumberValidity($mobile, isset($user->ID) ? $user->ID : false);
+
+        if (is_wp_error($validity)) {
+            $errors->add($validity->get_error_code(), $validity->get_error_message());
+        }
+
+        return $errors;
+    }
+
+    public function registerFieldInCheckoutBillingForm($fields)
+    {
+        $fields['billing'][$this->mobileField] = $this->getField();
+
+        return $fields;
+    }
+
+    private function getField()
+    {
+        return [
+            'label'       => __('Mobile', 'wp-sms'),
+            'placeholder' => __('Please enter your mobile number', 'wp-sms'),
+            'required'    => true,
+            'clear'       => false,
+            'type'        => 'text',
+            'class'       => array('wp-sms-input-mobile')
+        ];
     }
 }
