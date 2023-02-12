b0782d0bde967984435ff637d060948fd33240a6
Mostafa Soufi <mostafa@veronalabs.com>
2023-02-12 10:15:45 +0330

    Fix updating the customer number in order shop view

--- src/User/MobileFieldHandler/WooCommerceAddMobileFieldHandler.php
+++ src/User/MobileFieldHandler/WooCommerceAddMobileFieldHandler.php
@@ -28,6 +28,11 @@ class WooCommerceAddMobileFieldHandler
         // checkout billing address
         add_filter('woocommerce_checkout_fields', [$this, 'registerFieldInCheckoutBillingForm']);
         add_action('woocommerce_after_checkout_validation', [$this, 'validateMobileNumberInCheckoutCallback'], 10, 2);
+        add_action('woocommerce_checkout_order_processed', array($this, 'updateMobileNumberAfterPlaceTheOrder'), 10, 3);
+
+        // admin order billing address
+        add_filter('woocommerce_admin_billing_fields', [$this, 'registerFieldInAdminOrderBillingForm']);
+        add_action('woocommerce_process_shop_order_meta', array($this, 'updateCustomerMobileNumberAfterUpdateTheOrder'), 10, 2);
     }
 
     public function registerFieldInBillingForm($fields)
@@ -64,6 +69,26 @@ class WooCommerceAddMobileFieldHandler
         }
     }
 
+    /**
+     * @param $orderId
+     * @param $postedData
+     * @param $order \WC_Order
+     * @return void
+     */
+    public function updateMobileNumberAfterPlaceTheOrder($orderId, $postedData, $order)
+    {
+        file_put_contents('log-updateMobileNumberAfterPlaceTheOrder', print_r([
+            $orderId,
+            $postedData
+        ], 1));
+
+        $userMobile = isset($postedData[$this->mobileField]) ? sanitize_text_field($postedData[$this->mobileField]) : '';
+
+        if ($userMobile) {
+            $this->updateMobileNumber($orderId, $userMobile);
+        }
+    }
+
     public function registerFieldInAdminUserBillingForm($args)
     {
         $args['billing']['fields'][$this->mobileField] = array(
@@ -92,5 +117,51 @@ class WooCommerceAddMobileFieldHandler
             'type'        => 'text',
             'input_class' => array('wp-sms-input-mobile')
         ];
+    }
+
+    public function registerFieldInAdminOrderBillingForm($billingFields)
+    {
+        // Backward compatibility
+        if (empty($_GET['post'])) {
+            return;
+        }
+
+        $orderId = sanitize_text_field($_GET['post']);
+        $mobile  = get_post_meta($orderId, $this->mobileField, true);
+
+        $billingFields[$this->mobileField] = [
+            'label' => __('Mobile Number', 'wp-sms'),
+            'class' => 'wp-sms-input-mobile',
+            'value' => $mobile
+        ];
+
+        return $billingFields;
+    }
+
+    public function updateCustomerMobileNumberAfterUpdateTheOrder($orderId, $data)
+    {
+        if (isset($_POST['_billing_mobile'])) {
+            $this->updateMobileNumber($orderId, $_POST['_billing_mobile']);
+        }
+    }
+
+    /**
+     * @param $orderId
+     * @param $mobileNumber
+     * @return void
+     */
+    private function updateMobileNumber($orderId, $mobileNumber)
+    {
+        $mobileNumber = sanitize_text_field($mobileNumber);
+
+        // Update in order meta
+        update_post_meta($orderId, $this->mobileField, $mobileNumber);
+
+        // Update in user meta
+        $userId = get_post_meta($orderId, '_customer_user', true);
+
+        if ($userId and $userId != 0) {
+            update_user_meta($userId, $this->mobileField, $mobileNumber);
+        }
     }
 }
--- src/User/MobileFieldHandler/WooCommerceUsePhoneFieldHandler.php
+++ src/User/MobileFieldHandler/WooCommerceUsePhoneFieldHandler.php
@@ -6,6 +6,18 @@ class WooCommerceUsePhoneFieldHandler
 {
     public function register()
     {
-        // todo add_action
+        //add_filter('woocommerce_checkout_fields', array($this, 'edit_billing_phone'));
+    }
+
+     /**
+     * @param $fields
+     *
+     * @return mixed
+     */
+    public function edit_billing_phone($fields)
+    {
+        $fields['billing']['billing_phone']['id'] = 'wp-sms-input-mobile';
+
+        return $fields;
     }
 }
