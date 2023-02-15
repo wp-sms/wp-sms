2a7f9d8f9aa80cf6fc1b997dfc1199791bda0e5d
Farshid Rahimi <fo4rty@gmail.com>
2023-02-15 15:45:43 +0330

    Fix two minor offset issue

--- src/User/MobileFieldHandler/WooCommerceUsePhoneFieldHandler.php
+++ src/User/MobileFieldHandler/WooCommerceUsePhoneFieldHandler.php
@@ -32,14 +32,18 @@ class WooCommerceUsePhoneFieldHandler
      */
     public function modifyBillingPhoneAttributes($fields)
     {
-        $fields['billing']['billing_phone']['class'] = 'wp-sms-input-mobile';
+        if (isset($fields['billing']['billing_phone'])) {
+            $fields['billing']['billing_phone']['class'][] = 'wp-sms-input-mobile';
+        }
 
         return $fields;
     }
 
     public function modifyAdminBillingPhoneAttributes($fields)
     {
-        $fields['phone']['class'] = 'wp-sms-input-mobile';
+        if (isset($fields['phone']['class'])) {
+            $fields['phone']['class'][] = 'wp-sms-input-mobile';
+        }
 
         return $fields;
     }
