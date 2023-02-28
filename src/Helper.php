b21a9a957d6287f28c596c7e659d74666f26e21a
Hanie <dadvarhanieh98@gmail.com>
2023-02-28 17:05:58 +0330

    Revert getWooCommerceCustomersNumbers

--- src/Helper.php
+++ src/Helper.php
@@ -139,35 +139,33 @@ class Helper
      */
     public static function getWooCommerceCustomersNumbers($roles = [])
     {
-//        $fieldKey = self::getUserMobileFieldName();
-//        $args     = array(
-//            'meta_query' => array(
-//                array(
-//                    'key'     => $fieldKey,
-//                    'compare' => '>',
-//                ),
-//                array(
-//                    'key'     => 'billing_phone',
-//                    'compare' => '>',
-//                ),
-//            ),
-//            'fields'     => 'all_with_meta'
-//        );
-//
-//        if ($roles) {
-//            $args['role__in'] = $roles;
-//        }
-//
-//        $customers = get_users($args);
-//        $numbers   = array();
-//
-//        foreach ($customers as $customer) {
-//            $numbers[] = $customer->$fieldKey;
-//        }
-//
-//        return $numbers;
+        $fieldKey = self::getUserMobileFieldName();
+        $args     = array(
+            'meta_query' => array(
+                array(
+                    'key'     => $fieldKey,
+                    'compare' => '>',
+                ),
+                array(
+                    'key'     => 'billing_phone',
+                    'compare' => '>',
+                ),
+            ),
+            'fields'     => 'all_with_meta'
+        );
 
-        \WPSmsWooPro\Core\Helper::getCustomersNumbers();
+        if ($roles) {
+            $args['role__in'] = $roles;
+        }
+
+        $customers = get_users($args);
+        $numbers   = array();
+
+        foreach ($customers as $customer) {
+            $numbers[] = $customer->$fieldKey;
+        }
+
+        return $numbers;
     }
 
     /**
