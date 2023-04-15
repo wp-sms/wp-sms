25ae3b1b37988a08f17d731ff20e1319a54f8f7c
Mostafa Soufi <mostafa@veronalabs.com>
2023-04-15 16:17:09 +0330

    Unit Test: Search WooComerce mobile number by order ID

--- tests/wpunit/MobileNumberUtilsTest.php
+++ tests/wpunit/MobileNumberUtilsTest.php
@@ -50,4 +50,55 @@ class MobileNumberUtilsTest extends \Codeception\TestCase\WPTestCase
             $this->assertIsInt($userId, $user->ID);
         }
     }
+
+    public function testSearchWooCommereCustomerMobileNumberByOrderId()
+    {
+        // create user
+        $userId = static::factory()->user->create();
+
+        // Update mobile field option
+        Option::updateOption('add_mobile_field', 'use_phone_field_in_wc_billing');
+
+        // Add mobile to user
+        update_user_meta($userId, Helper::getUserMobileFieldName(), '4186578565');
+
+        $order = wc_create_order();
+        $order->set_billing_first_name('Mary');
+        $order->set_billing_last_name('Christensen');
+        $order->set_billing_address_1('179 Allison Avenue');
+        $order->set_billing_postcode('3152');
+        $order->set_billing_email('marykchristensen@armyspy.com');
+        $order->set_billing_country('US');
+        $order->set_billing_city('VA');
+        $order->set_billing_phone('4186578565');
+        $order->set_customer_id($userId);
+        $order->save();
+        $order->save_meta_data();
+
+        $customerMobileNumber = Helper::getWooCommerceCustomerNumberByOrderId($order->get_id());
+
+        $this->assertStringContainsString('4186578565', $customerMobileNumber);
+    }
+
+    public function testSearchWooCommereGuestMobileNumberByOrderId()
+    {
+        // Update mobile field option
+        Option::updateOption('add_mobile_field', 'use_phone_field_in_wc_billing');
+
+        $order = wc_create_order();
+        $order->set_billing_first_name('Goldie');
+        $order->set_billing_last_name('Ramos');
+        $order->set_billing_address_1('4600 Woodhill Avenue');
+        $order->set_billing_postcode('21030');
+        $order->set_billing_email('goldierramos@armyspy.com');
+        $order->set_billing_country('US');
+        $order->set_billing_city('MD');
+        $order->set_billing_phone('4186578566');
+        $order->save();
+        $order->save_meta_data();
+
+        $customerMobileNumber = Helper::getWooCommerceCustomerNumberByOrderId($order->get_id());
+
+        $this->assertStringContainsString('4186578566', $customerMobileNumber);
+    }
 }
