de93fa637f4cb65868870cca3d6b80534ca2f91e
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2023-06-07 08:57:44 +0330

    Fix single pixel gap issue on header

--- assets/blocks/Subscribe/index.asset.php
+++ assets/blocks/Subscribe/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e57a63e1a804fffdac0a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'acd8318aa7b072337169');
