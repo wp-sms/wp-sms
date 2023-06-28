5c5ead3e1f1c1d3ba11cd8a2e5aa604f7b6854d9
Mostafa Soufi <mst404@gmail.com>
2023-06-29 01:44:10 +0330

    Fix notice name

--- src/Notice/NoticeManager.php
+++ src/Notice/NoticeManager.php
@@ -47,7 +47,7 @@ class NoticeManager extends AbstractNotice
     {
         $this->registerNotice('woocommerce_mobile_field', __('You need to configure the Mobile field option in General settings to send SMS to customers.', 'wp-sms'), true, 'admin.php?page=wp-sms-settings&tab=pro_woocommerce');
         $this->registerNotice('login_mobile_field', __('You need to configure the Mobile field option to use login with SMS functionality.', 'wp-sms'), true, 'admin.php?page=wp-sms-settings&tab=pro_wordpress');
-        $this->registerNotice('marketing_newsletter1', sprintf(__('Stay informed and receive exclusive offers, <a href="%s" target="_blank">Subscribe to our newsletter here</a>!', 'wp-sms'), 'https://dashboard.mailerlite.com/forms/421827/86962232715379904/share'), true, 'admin.php?page=wp-sms-settings');
+        $this->registerNotice('marketing_newsletter', sprintf(__('Stay informed and receive exclusive offers, <a href="%s" target="_blank">Subscribe to our newsletter here</a>!', 'wp-sms'), 'https://dashboard.mailerlite.com/forms/421827/86962232715379904/share'), true, 'admin.php?page=wp-sms-settings');
     }
 
     /**
