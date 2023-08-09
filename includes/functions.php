883bbd21c1ab55a2fc7452b6a9008060b0a0f7a8
ighorbani <imanghorbani.ir@gmail.com>
2023-08-09 17:11:45 +0330

    Add Feature _ Support Group Id for the Subscribe Shortcide

--- includes/functions.php
+++ includes/functions.php
@@ -425,7 +425,7 @@ function wp_sms_subscriber_form($attributes = array())
             'gdpr_compliance'                      => wp_sms_get_option('gdpr_compliance'),
             'subscribe_form_gdpr_confirm_checkbox' => wp_sms_get_option('newsletter_form_gdpr_confirm_checkbox'),
             'subscribe_form_gdpr_text'             => wp_sms_get_option('newsletter_form_gdpr_text'),
-            'get_group_result'                     => \WP_SMS\Newsletter::getGroups(wp_sms_get_option('newsletter_form_specified_groups')),
+            'get_group_result'                     => isset($attributes['groups']) ? $attributes['groups'] : \WP_SMS\Newsletter::getGroups(wp_sms_get_option('newsletter_form_specified_groups')),
         ]
     );
 }
