985475c653619d17a1185988455f414798547ff3
Mostafa Soufi <mst404@gmail.com>
2023-09-19 13:42:51 +0330

    i18 for mail delivery issue

--- includes/templates/email/email-sms-delivery-issue.php
+++ includes/templates/email/email-sms-delivery-issue.php
@@ -1,13 +1,13 @@
-<p>WP SMS encountered an issue while sending SMS messages to the following numbers:</p>
+<p><?php _e('WP SMS encountered an issue while sending SMS messages to the following numbers:', 'wp-sms'); ?></p>
 
 <code><?php echo implode(', ', $to); ?></code>
 
-<p><b>SMS Content:</b></p>
+<p><b><?php _e('SMS Content', 'wp-sms'); ?>:</b></p>
 
 <?php echo $message; ?>
 
-<p><b>API Response:</b></p>
+<p><b><?php _e('API Response', 'wp-sms'); ?>:</b></p>
 
-<p><code><?php echo $response; ?></code></p>
+<p><code><?php print_r($response); ?></code></p>
 
-<p>To address this issue, please verify the SMS configuration in the WP SMS settings page. Ensure that the credentials are correct, and you have entered the recipient numbers accurately, with or without the country code.</p>
+<p><?php _e('To address this issue, please verify the SMS configuration in the WP SMS settings page. Ensure that the credentials are correct, and you have entered the recipient numbers accurately, with or without the country code.', 'wp-sms'); ?></p>
