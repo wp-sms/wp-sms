d0555bdd4d37c36d53d6714abc5ce8bfb15eb611
Mostafa Soufi <mst404@gmail.com>
2023-12-01 16:26:40 +0100

    Backward compatibility with new custom WooCommerce order table.

--- src/Services/WooCommerce/OrderViewManager.php
+++ src/Services/WooCommerce/OrderViewManager.php
@@ -25,7 +25,13 @@ class OrderViewManager
         global $sms;
         $nonce = wp_create_nonce('wp_rest');
 
-        $order_id        = isset($_GET['id']) ? sanitize_text_field($_GET['id']) : 0;
+        // Backward compatibility with new custom WooCommerce order table.
+        if (isset($_GET['page']) && $_GET['page'] == 'wc-orders') {
+            $order_id = sanitize_text_field($_GET['id']);
+        } else {
+            $order_id = sanitize_text_field($_GET['post']);
+        }
+
         $customer_mobile = \WP_SMS\Helper::getWooCommerceCustomerNumberByOrderId($order_id);
 
         wp_enqueue_script('wpsms-woocommerce-admin', WP_SMS_URL . 'assets/js/admin-order-view.js', ['jquery', 'jquery-ui-spinner'], WP_SMS_VERSION);
