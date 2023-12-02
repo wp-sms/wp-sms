ab2cd3a9e9f21889600b7f75263ec6076215e4f6
Mostafa Soufi <mst404@gmail.com>
2023-12-02 22:55:01 +0100

    Build front styles

--- assets/blocks/SendSms/index.asset.php
+++ assets/blocks/SendSms/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3b29085e50f0ba4b2a36');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7e2a394bbf1e1c8e3bcf');
--- assets/blocks/Subscribe/index.asset.php
+++ assets/blocks/Subscribe/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b4516bc090ac1b81d911');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2b954593e2ea819ca1b7');
