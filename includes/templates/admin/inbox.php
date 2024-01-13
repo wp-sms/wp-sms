363de18655bbcb323cabde6c7af7bd32378958ba
Mostafa Soufi <mst404@gmail.com>
2024-01-13 17:54:00 +0330

    Update Two-Way promottion box

--- includes/templates/admin/inbox.php
+++ includes/templates/admin/inbox.php
@@ -4,15 +4,18 @@
         <img class="background-img" src="<?php echo WP_SMS_URL . '/assets/images/blurred-inbox.png'; ?>" alt="">
         <div class="promotion-modal">
             <h3 class="promotion-modal__title"><?php _e('View Inbox / Incoming Messages', 'wp-sms'); ?></h3>
-            <p class="promotion-modal__desc"><?php _e('This feature is available through the <b>WP SMS Two Way</b> add-on which allows you to receive messages and do actions with them such as unsubscribe, cancel order, etc.'); ?></p>
+            <h3 class="promotion-modal__screenshot">
+                <img src="<?php echo WP_SMS_URL . '/assets/images/wp-sms-two-way-chagemode.jpg'; ?>" alt="">
+            </h3>
+            <p class="promotion-modal__desc"><?php _e('Introducing Chat Mode in <b>WP SMS Two Way</b>: Now respond to messages with actions like unsubscribe or cancel orders, compatible with WooCommerce and Subscriber.'); ?></p>
             <div class="promotion-modal__features">
                 <div class="promotion-modal__feature__col">
-                    <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Store Receive Messages', 'wp-sms'); ?></div>
+                    <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Store Received Messages', 'wp-sms'); ?></div>
                     <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Create Specific Commands', 'wp-sms'); ?></div>
                     <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Do Actions', 'wp-sms'); ?></div>
                 </div>
                 <div class="promotion-modal__feature__col">
-                    <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Search Messages', 'wp-sms'); ?></div>
+                    <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Show In Chat Mode', 'wp-sms'); ?></div>
                     <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('WooCommerce Compatibility', 'wp-sms'); ?></div>
                     <div class="promotion-modal__features__item"><span class="dashicons dashicons-saved"></span> <?php _e('Subscriber Compatibility', 'wp-sms'); ?></div>
                 </div>
