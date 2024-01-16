64dd9f62b70e9760e19d8472109a8ed7a7b55dde
Mostafa Soufi <mst404@gmail.com>
2024-01-16 21:50:42 +0330

    Update textanywhere

--- includes/gateways/class-wpsms-gateway-textanywhere.php
+++ includes/gateways/class-wpsms-gateway-textanywhere.php
@@ -2,21 +2,63 @@
 
 namespace WP_SMS\Gateway;
 
+use Exception;
+use WP_Error;
+
 class textanywhere extends \WP_SMS\Gateway
 {
     private $wsdl_link = "https://www.textapp.net/webservice/httpservice.aspx";
+    private $wsdl_link_new = "https://api.textanywhere.com/API/v1.0/REST";
     public $tariff = "http://www.textanywhere.net/";
     public $unitrial = false;
     public $unit;
     public $flash = "disable";
     public $isflash = false;
+    public $accountType = 'new';
+    public $apiPassword;
 
     public function __construct()
     {
         parent::__construct();
         $this->has_key        = true;
-        $this->help           = 'You should use the Client_Id and Client_Pass instead API username and API password. The Client_Id and Client_Pass value can be found by logging in to your online account, and clicking on the ADMIN PANEL button.';
         $this->validateNumber = "For example, mobile number (07836) 123-456 would be formatted as +447836123456.";
+        $this->gatewayFields  = [
+            'username'    => [
+                'id'   => 'gateway_username',
+                'name' => 'Username',
+                'desc' => 'Enter your account username',
+            ],
+            'password'    => [
+                'id'   => 'gateway_password',
+                'name' => 'Password',
+                'desc' => 'Enter your account password',
+            ],
+            'apiPassword' => [
+                'id'   => 'gateway_api_password',
+                'name' => 'API Password',
+                'desc' => 'Enter your API password get from Account -> API & IPs (If your type of account is new)',
+            ],
+            'has_key'     => [
+                'id'   => 'gateway_key',
+                'name' => 'API Key',
+                'desc' => 'Enter the account Token, get from Account -> API & IPs (If your type of account is new)',
+            ],
+            'from'        => [
+                'id'   => 'gateway_sender_id',
+                'name' => 'Sender ID type',
+                'desc' => 'Please select what is your Sender ID type',
+            ],
+            'accountType' => [
+                'id'      => 'gateway_account_type',
+                'name'    => 'Account Type',
+                'desc'    => 'Please select what is your account type.',
+                'type'    => 'select',
+                'options' => [
+                    'old' => 'Old',
+                    'new' => 'New',
+                ]
+            ],
+        ];
     }
 
     public function SendSMS()
@@ -49,86 +91,159 @@ class textanywhere extends \WP_SMS\Gateway
          */
         $this->msg = apply_filters('wp_sms_msg', $this->msg);
 
-        // Get the credit.
-        $credit = $this->GetCredit();
+        if ($this->accountType == 'new') {
 
-        // Check gateway credit
-        if (is_wp_error($credit)) {
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $credit->get_error_message(), 'error');
+            try {
 
-            return $credit;
-        }
+                $token  = $this->getToken();
+                $params = array(
+                    'headers' => array(
+                        'Content-Type' => 'application/json',
+                        'user_key'     => $token[0],
+                        'Access_token' => $token[1],
+                    ),
+                    'body'    => json_encode([
+                        'message_type' => 'GP',
+                        'message'      => $this->msg,
+                        'recipient'    => $this->to,
+                        'sender'       => $this->from,
+                    ])
+                );
 
-        $to      = implode(",", $this->to);
-        $message = urlencode($this->msg);
+                $response = $this->request('POST', "{$this->wsdl_link_new}/sms", [], $params, false);
 
-        $response = wp_remote_get($this->wsdl_link . "?method=sendsms&externallogin=" . $this->username . "&password=" . $this->password . "&clientbillingreference=myclientbillingreference&clientmessagereference=myclientmessagereference&originator=" . $this->from . "&destinations=" . $to . "&body=" . $message . "&validity=72&charactersetid=2&replymethodid=1");
+                if (isset($response->error_message)) {
+                    throw new Exception($response->error_message);
+                }
 
-        // Check gateway credit
-        if (is_wp_error($response)) {
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $response->get_error_message(), 'error');
-
-            return new \WP_Error('send-sms', $response->get_error_message());
-        }
-
-        $result = $this->XML2Array($response['body']);
-
-        if (isset($result['Transaction']['Code']) and $result['Transaction']['Code'] == '1') {
-
-            if (isset($result['Destinations']['Destination']['Code']) and $result['Destinations']['Destination']['Code'] == '1') {
-                // Log the result
-                $this->log($this->from, $this->msg, $this->to, $result);
+                //log the result
+                $this->log($this->from, $this->msg, $this->to, $response);
 
                 /**
                  * Run hook after send sms.
                  *
-                 * @param string $result result output.
+                 * @param string $response result output.
                  * @since 2.4
                  *
                  */
-                do_action('wp_sms_send', $result);
+                do_action('wp_sms_send', $response);
 
-                return $result;
+                return $response;
+
+            } catch (Exception $e) {
+                $this->log($this->from, $this->msg, $this->to, $e->getMessage(), 'error');
+
+                return new WP_Error('send-sms', $e->getMessage());
+            }
+
+        } else {
+
+            $to      = implode(",", $this->to);
+            $message = urlencode($this->msg);
+
+            $response = wp_remote_get($this->wsdl_link . "?method=sendsms&externallogin=" . $this->username . "&password=" . $this->password . "&clientbillingreference=myclientbillingreference&clientmessagereference=myclientmessagereference&originator=" . $this->from . "&destinations=" . $to . "&body=" . $message . "&validity=72&charactersetid=2&replymethodid=1");
+
+            // Check gateway credit
+            if (is_wp_error($response)) {
+                // Log the result
+                $this->log($this->from, $this->msg, $this->to, $response->get_error_message(), 'error');
+
+                return new WP_Error('send-sms', $response->get_error_message());
+            }
+
+            $result = $this->XML2Array($response['body']);
+
+            if (isset($result['Transaction']['Code']) and $result['Transaction']['Code'] == '1') {
+
+                if (isset($result['Destinations']['Destination']['Code']) and $result['Destinations']['Destination']['Code'] == '1') {
+                    // Log the result
+                    $this->log($this->from, $this->msg, $this->to, $result);
+
+                    /**
+                     * Run hook after send sms.
+                     *
+                     * @param string $result result output.
+                     * @since 2.4
+                     *
+                     */
+                    do_action('wp_sms_send', $result);
+
+                    return $result;
+                } else {
+                    // Log the result
+                    $this->log($this->from, $this->msg, $this->to, $this->get_error_message($result['Destinations']['Destination']['Code']), 'error');
+
+                    return new WP_Error('send-sms', $this->get_error_message($result['Destinations']['Destination']['Code']));
+                }
             } else {
                 // Log the result
-                $this->log($this->from, $this->msg, $this->to, $this->get_error_message($result['Destinations']['Destination']['Code']), 'error');
+                $this->log($this->from, $this->msg, $this->to, $result['Transaction']['Description'], 'error');
 
-                return new \WP_Error('send-sms', $this->get_error_message($result['Destinations']['Destination']['Code']));
+                return new WP_Error('send-sms', $result['Transaction']['Description']);
             }
-        } else {
-            // Log the result
-            $this->log($this->from, $this->msg, $this->to, $result['Transaction']['Description'], 'error');
 
-            return new \WP_Error('send-sms', $result['Transaction']['Description']);
         }
     }
 
     public function GetCredit()
     {
-        // Check api key and password
-        if (!$this->has_key && !$this->password) {
-            return new \WP_Error('account-credit', __('API username or API password is not entered.', 'wp-sms'));
-        }
+        if ($this->accountType == 'new') {
 
-        $response = wp_remote_get($this->wsdl_link . "?method=GetCreditsLeft&externallogin=" . $this->username . "&password=" . $this->password);
+            try {
+                if (!$this->username || !$this->apiPassword) {
+                    throw new Exception(__('Username and API password are required.', 'wp-sms'));
+                }
 
-        // Check gateway credit
-        if (is_wp_error($response)) {
-            return new \WP_Error('account-credit', $response->get_error_message());
-        }
+                $token  = $this->getToken();
+                $params = array(
+                    'headers' => array(
+                        'Content-Type' => 'application/json',
+                        'user_key'     => $token[0],
+                        'Access_token' => $token[1],
+                    )
+                );
 
-        if (!function_exists('simplexml_load_string')) {
-            return new \WP_Error('account-credit', sprintf(__('The <code>%s</code> function is not active in your server.', 'wp-sms'), 'simplexml_load_string'));
-        }
+                $response = $this->request('GET', "{$this->wsdl_link_new}/status", [], $params, false);
 
-        $result = $this->XML2Array($response['body']);
+                if (isset($response->error_message)) {
+                    throw new Exception($response->error_message);
+                }
 
-        if (isset($result['Transaction']['Code']) and $result['Transaction']['Code'] == '1') {
-            return $result['CreditLeft'];
+                $creditParts = [];
+                foreach ($response->sms as $sms) {
+                    $creditParts[] = "{$sms->type}: {$sms->quantity}";
+                }
+
+                return implode(", ", $creditParts);
+
+            } catch (Exception $e) {
+                return new WP_Error('account-credit', $e->getMessage());
+            }
+
         } else {
-            return new \WP_Error('account-credit', $result['Transaction']['Description']);
+            // Check api key and password
+            if (!$this->has_key && !$this->password) {
+                return new WP_Error('account-credit', __('API username or API password is not entered.', 'wp-sms'));
+            }
+
+            $response = wp_remote_get($this->wsdl_link . "?method=GetCreditsLeft&externallogin=" . $this->username . "&password=" . $this->password);
+
+            // Check gateway credit
+            if (is_wp_error($response)) {
+                return new WP_Error('account-credit', $response->get_error_message());
+            }
+
+            if (!function_exists('simplexml_load_string')) {
+                return new WP_Error('account-credit', sprintf(__('The <code>%s</code> function is not active in your server.', 'wp-sms'), 'simplexml_load_string'));
+            }
+
+            $result = $this->XML2Array($response['body']);
+
+            if (isset($result['Transaction']['Code']) and $result['Transaction']['Code'] == '1') {
+                return $result['CreditLeft'];
+            } else {
+                return new WP_Error('account-credit', $result['Transaction']['Description']);
+            }
         }
     }
 
@@ -138,9 +253,7 @@ class textanywhere extends \WP_SMS\Gateway
      *
      * @return array
      */
-    private function XML2Array(
-        $xml, $recursive = false
-    )
+    private function XML2Array($xml, $recursive = false)
     {
         if (!$recursive) {
             $array = simplexml_load_string($xml);
@@ -181,6 +294,30 @@ class textanywhere extends \WP_SMS\Gateway
             default:
                 return sprintf('Error code: %s, See message codes: http://developer.textapp.net/HTTPService/TransactionCodes.aspx', $error_code);
                 break;
+        }
+    }
+
+    private function getToken()
+    {
+        $params = array(
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode($this->username . ':' . $this->apiPassword),
+                'Content-Type'  => 'application/json',
+            ]
+        );
+
+        $response = $this->request('GET', "{$this->wsdl_link_new}/token", [], $params, false);
+
+        if (isset($response->error_message)) {
+            throw new Exception($response->error_message);
+        }
+
+        $parts = explode(";", $response);
+
+        if (is_array($parts)) {
+            return $parts;
+        } else {
+            throw new Exception('Invalid get token response ' . $response);
         }
     }
 }
