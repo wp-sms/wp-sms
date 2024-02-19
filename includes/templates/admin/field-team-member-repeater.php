bbb867845d939052f2bac6e49c8a9ad15b0fd5bd
foadadeli <foad.adeli@gmail.com>
2024-02-19 14:53:07 +0330

    Fix minor issues

--- includes/templates/admin/field-team-member-repeater.php
+++ includes/templates/admin/field-team-member-repeater.php
@@ -11,7 +11,7 @@
                     $member_contact_value   = isset($data['member_contact_value']) ? $data['member_contact_value'] : '';
                 ?>
                 <div class="repeater-item" data-repeater-item>
-                    <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden;">
+                    <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden; padding: 5px 0;">
                         <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
                             <input type="text" placeholder="<?php _e('Name', 'wp-sms') ?>" name="member_name" value="<?php echo esc_attr($member_name) ?>" style="display: block; width: 99%;" />
                             <p class="description"><?php _e('Enter the team member\'s name.', 'wp-sms') ?></p>
@@ -55,7 +55,7 @@
             <?php endforeach; ?>
         <?php else : ?>
             <div class="repeater-item" data-repeater-item>
-                <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden;">
+                <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden; padding: 5px 0;">
                     <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
                         <input type="text" placeholder="<?php _e('Name', 'wp-sms') ?>" name="member_name" style="display: block; width: 99%;" />
                         <p class="description"><?php _e('Enter the team member\'s name.', 'wp-sms') ?></p>
@@ -67,7 +67,7 @@
                     <div style="margin-bottom: 15px; display: block; width: 100%; float: left;">
                         <div style="display: block; width: 100%;">
                             <input id="member_photo_field" type="text" class="wpsms_settings_upload_field" placeholder="<?php _e('Photo', 'wp-sms') ?>" name="member_photo"/>
-                            <span><input type="button" data-target="member_photo_field" class="wpsms_settings_upload_button button button-secondary" /></span>
+                            <span><input type="button" data-target="member_photo_field" class="wpsms_settings_upload_button button button-secondary" value="<?php _e('Upload', 'wp-sms') ?>"/></span>
                         </div>
                         <p class="description"><?php _e('Upload a photo for the team member.', 'wp-sms') ?></p>
                     </div>
@@ -79,11 +79,11 @@
                     <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
                         <select name="member_contact_type" style="display: block; width: 99%;">
                             <option value=""><?php _e('Choose Contact Type', 'wp-sms') ?></option>
-                            <option value="whatsapp" <?php echo ($member_contact_type == 'whatsapp') ? 'selected' : '' ?>><?php _e('WhatsApp', 'wp-sms') ?></option>
-                            <option value="call" <?php echo ($member_contact_type == 'call') ? 'selected' : '' ?>><?php _e('Phone Call', 'wp-sms') ?></option>
-                            <option value="facebook" <?php echo ($member_contact_type == 'facebook') ? 'selected' : '' ?>><?php _e('Facebook Messenger', 'wp-sms') ?></option>
-                            <option value="telegram" <?php echo ($member_contact_type == 'telegram') ? 'selected' : '' ?>><?php _e('Telegram', 'wp-sms') ?></option>
-                            <option value="sms" <?php echo ($member_contact_type == 'sms') ? 'selected' : '' ?>><?php _e('SMS', 'wp-sms') ?></option>
+                            <option value="whatsapp"><?php _e('WhatsApp', 'wp-sms') ?></option>
+                            <option value="call"><?php _e('Phone Call', 'wp-sms') ?></option>
+                            <option value="facebook"><?php _e('Facebook Messenger', 'wp-sms') ?></option>
+                            <option value="telegram"><?php _e('Telegram', 'wp-sms') ?></option>
+                            <option value="sms"><?php _e('SMS', 'wp-sms') ?></option>
                         </select>
                         <p class="description"><?php _e('Choose the team member\'s contact method (WhatsApp, Phone Call, Facebook Messenger, Telegram, SMS).', 'wp-sms') ?></p>
                     </div>
