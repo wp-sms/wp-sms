4f3a11ad53c17f8dd156c5c47baf8a62fd9317f6
foadadeli <foad.adeli@gmail.com>
2024-02-19 16:46:44 +0330

    Update repeater availability checkbox to text

--- includes/templates/admin/field-team-member-repeater.php
+++ includes/templates/admin/field-team-member-repeater.php
@@ -6,34 +6,33 @@
                     $member_name            = isset($data['member_name']) ? $data['member_name'] : '';
                     $member_role            = isset($data['member_role']) ? $data['member_role'] : '';
                     $member_photo           = isset($data['member_photo']) ? $data['member_photo'] : '';
-                    $member_availability    = isset($data['member_availability']) ? 'checked' : '';
+                    $member_availability    = isset($data['member_availability']) ? $data['member_availability'] : '';
                     $member_contact_type    = isset($data['member_contact_type']) ? $data['member_contact_type'] : '';
                     $member_contact_value   = isset($data['member_contact_value']) ? $data['member_contact_value'] : '';
                 ?>
                 <div class="repeater-item" data-repeater-item>
-                    <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden; padding: 5px 0;">
-                        <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
+                    <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 20px 40px; border-bottom: 1px solid #ccc; margin-bottom: 15px;">
+                        <div>
                             <input type="text" placeholder="<?php _e('Name', 'wp-sms') ?>" name="member_name" value="<?php echo esc_attr($member_name) ?>" style="display: block; width: 99%;" />
                             <p class="description"><?php _e('Enter the team member\'s name.', 'wp-sms') ?></p>
                         </div>
-                        <div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
+                        <div>
                             <input type="text" placeholder="<?php _e('Role', 'wp-sms') ?>" name="member_role" value="<?php echo esc_attr($member_role) ?>" style="display: block; width: 99%;" />
                             <p class="description"><?php _e('Type in the team member\'s role or department.', 'wp-sms') ?></p>
                         </div>
-                        <div style="margin-bottom: 15px; display: block; width: 100%; float: left;">
-                            <div style="display: block; width: 100%;">
-                                <input id="member_photo_field[<?php echo esc_attr($key) ?>]" type="text" class="wpsms_settings_upload_field" placeholder="<?php _e('Photo', 'wp-sms') ?>" value="<?php echo esc_attr($member_photo) ?>" name="member_photo"/>
+                        <div>
+                            <div style="display: flex; gap: 5px; justify-content: space-between; width: 100%;">
+                                <input id="member_photo_field[<?php echo esc_attr($key) ?>]" type="text" class="wpsms_settings_upload_field" placeholder="<?php _e('Photo', 'wp-sms') ?>" value="<?php echo esc_attr($member_photo) ?>" name="member_photo" style="width: 80%"/>
                                 <span><input type="button" data-target="member_photo_field[<?php echo esc_attr($key) ?>]" class="wpsms_settings_upload_button button button-secondary" value="<?php _e('Upload', 'wp-sms') ?>"/></span>
                             </div>
                             <p class="description"><?php _e('Upload a photo for the team member.', 'wp-sms') ?></p>
                         </div>
-                        <div style="display: block; width: 100%; float: left; margin-bottom: 15px;">
-                            <input id="member-availability[<?php echo esc_attr($key) ?>]" type="checkbox" <?php echo esc_attr($member_availability) ?> name="member_availability"/>
-                            <label for="member-availability[<?php echo esc_attr($key) ?>]"><?php _e('Availability', 'wp-sms') ?></label>
+                        <div>
+                            <input type="text" placeholder="<?php _e('Availability Text', 'wp-sms') ?>" value="<?php echo esc_attr($member_availability) ?>" name="member_availability" style="width: 100%;" />
                             <p class="description"><?php _e('Indicate when the team member is available to chat.', 'wp-sms') ?></p>
                         </div>
-                        <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
-                            <select name="member_contact_type" style="display: block; width: 99%;">
+                        <div>
+                            <select name="member_contact_type" style="display: block; min-width: 100%;">
                                 <option value=""><?php _e('Contact Type', 'wp-sms') ?></option>
                                 <option value="whatsapp" <?php selected($member_contact_type, 'whatsapp') ?>><?php _e('WhatsApp', 'wp-sms') ?></option>
                                 <option value="call" <?php selected($member_contact_type, 'call') ?>><?php _e('Phone Call', 'wp-sms') ?></option>
@@ -43,7 +42,7 @@
                             </select>
                             <p class="description"><?php _e('Choose the team member\'s contact method (WhatsApp, Phone Call, Facebook Messenger, Telegram, SMS).', 'wp-sms') ?></p>
                         </div>
-                        <div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
+                        <div>
                             <input type="text" placeholder="<?php _e('Contact Info', 'wp-sms') ?>" name="member_contact_value" value="<?php echo esc_attr($member_contact_value) ?>" style="display: block; width: 99%;"/>
                             <p class="description"><?php _e('Provide the team member\'s contact details for the selected method.', 'wp-sms') ?></p>
                         </div>
@@ -55,29 +54,28 @@
             <?php endforeach; ?>
         <?php else : ?>
             <div class="repeater-item" data-repeater-item>
-                <div style="display: block; width: 100%; margin-bottom: 15px; border-bottom: 1px solid #ccc; overflow: hidden; padding: 5px 0;">
-                    <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
-                        <input type="text" placeholder="<?php _e('Name', 'wp-sms') ?>" name="member_name" style="display: block; width: 99%;" />
+                <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 20px 40px; border-bottom: 1px solid #ccc; margin-bottom: 15px;">
+                    <div>
+                        <input type="text" placeholder="<?php _e('Name', 'wp-sms') ?>" name="member_name" style="display: block; width: 100%;" />
                         <p class="description"><?php _e('Enter the team member\'s name.', 'wp-sms') ?></p>
                     </div>
-                    <div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
-                        <input type="text" placeholder="<?php _e('Role', 'wp-sms') ?>" name="member_role" style="display: block; width: 99%;" />
+                    <div>
+                        <input type="text" placeholder="<?php _e('Role', 'wp-sms') ?>" name="member_role" style="display: block; width: 100%;" />
                         <p class="description"><?php _e('Type in the team member\'s role or department.', 'wp-sms') ?></p>
                     </div>
-                    <div style="margin-bottom: 15px; display: block; width: 100%; float: left;">
-                        <div style="display: block; width: 100%;">
-                            <input id="member_photo_field" type="text" class="wpsms_settings_upload_field" placeholder="<?php _e('Photo', 'wp-sms') ?>" name="member_photo"/>
+                    <div>
+                        <div style="display: flex; gap: 5px; justify-content: space-between; width: 100%;">
+                            <input id="member_photo_field" type="text" class="wpsms_settings_upload_field" placeholder="<?php _e('Photo', 'wp-sms') ?>" name="member_photo" style="width: 80%"/>
                             <span><input type="button" data-target="member_photo_field" class="wpsms_settings_upload_button button button-secondary" value="<?php _e('Upload', 'wp-sms') ?>"/></span>
                         </div>
                         <p class="description"><?php _e('Upload a photo for the team member.', 'wp-sms') ?></p>
                     </div>
-                    <div style="display: block; width: 100%; float: left; margin-bottom: 15px;">
-                        <input id="member-availability" type="checkbox" name="member_availability"/>
-                        <label for="member-availability"><?php _e('Availability', 'wp-sms') ?></label>
+                    <div>
+                        <input type="text" placeholder="<?php _e('Availability Text', 'wp-sms') ?>" name="member_availability" style="width: 100%;" />
                         <p class="description"><?php _e('Indicate when the team member is available to chat.', 'wp-sms') ?></p>
                     </div>
-                    <div style="display: block; width: 48%; float: left; margin-bottom: 15px;">
-                        <select name="member_contact_type" style="display: block; width: 99%;">
+                    <div>
+                        <select name="member_contact_type" style="display: block; min-width: 100%;">
                             <option value=""><?php _e('Choose Contact Type', 'wp-sms') ?></option>
                             <option value="whatsapp"><?php _e('WhatsApp', 'wp-sms') ?></option>
                             <option value="call"><?php _e('Phone Call', 'wp-sms') ?></option>
@@ -87,8 +85,8 @@
                         </select>
                         <p class="description"><?php _e('Choose the team member\'s contact method (WhatsApp, Phone Call, Facebook Messenger, Telegram, SMS).', 'wp-sms') ?></p>
                     </div>
-                    <div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
-                        <input type="text" placeholder="<?php _e('Contact Info', 'wp-sms') ?>" name="member_contact_value" style="display: block; width: 99%;"/>
+                    <div>
+                        <input type="text" placeholder="<?php _e('Contact Info', 'wp-sms') ?>" name="member_contact_value" style="display: block; width: 100%;"/>
                         <p class="description"><?php _e('Provide the team member\'s contact details for the selected method.', 'wp-sms') ?></p>
                     </div>
                     <div>
