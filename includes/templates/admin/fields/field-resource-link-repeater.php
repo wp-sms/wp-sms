4d194d5626c1ec0fc28f67690d8c78bd427af1ff
Mostafa Soufi <mst404@gmail.com>
2024-02-19 17:53:39 +0330

    Tweak improvements

--- includes/templates/admin/fields/field-resource-link-repeater.php
+++ includes/templates/admin/fields/field-resource-link-repeater.php
@@ -12,7 +12,7 @@
                             <input placeholder="<?php _e('Resource Link Title', 'wp-sms') ?>" type="text" name="resource_link_title" style="display: block; width: 99%;" value="<?php echo esc_attr($resource_link_title) ?>" />
                         </div>
                         <div style="display: block; width: 48%; float: right; margin-bottom: 15px;">
-                            <input placeholder="<?php _e('Resource Link URL', 'wp-sms') ?>" type="text" name="resource_link_url" style="display: block; width: 99%;" value="<?php echo esc_attr($resource_link_url) ?>" />
+                            <input placeholder="<?php _e('Resource Link URL', 'wp-sms') ?>" type="text" name="resource_link_url" style="display: block; width: 99%;" value="<?php echo esc_url($resource_link_url) ?>" />
                         </div>
                         <div>
                             <input type="button" value="<?php _e('Delete', 'wp-sms') ?>" class="button" style="margin-bottom: 15px;" data-repeater-delete/>
