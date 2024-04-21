b366238d6f0739d6742fb47fbc7ecd489fdf8943
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2024-04-21 18:27:08 +0330

    minor fixes

--- assets/blocks/OrderNotification/index.asset.php
+++ assets/blocks/OrderNotification/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => '9ad9a65ab98cdb213cd4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => '3b49f740cfa2dfdc6582');
