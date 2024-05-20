07841a1e859f09331c3646cbe3131ead95acc8ef
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2024-05-20 14:23:41 +0330

    Remove unused codes

--- assets/js/intel/intel-script.js
+++ assets/js/intel/intel-script.js
@@ -1,45 +1,3 @@
-// jQuery(document).ready(function () {
-//     setTimeout(() => init(), 500);
-// });
-//
-// function init() {
-//     var inputTells = document.querySelectorAll(".wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone, #billing-phone, #wp-sms-input-mobile, .user-mobile-wrap #mobile");
-//
-//     for (var i = 0; i < inputTells.length; i++) {
-//         if (inputTells[i] && inputTells[i].nodeName == 'INPUT') {
-//
-//             inputTells[i].setAttribute('dir', 'ltr')
-//             window.intlTelInput(inputTells[i], {
-//                 autoInsertDialCode: true,
-//                 onlyCountries: wp_sms_intel_tel_input.only_countries,
-//                 preferredCountries: wp_sms_intel_tel_input.preferred_countries,
-//                 //autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
-//                 nationalMode: false,
-//                 utilsScript: wp_sms_intel_tel_input.util_js,
-//                 formatOnDisplay: false,
-//                 //initialCountry: 'auto'
-//             });
-//         }
-//     }
-//
-//     var inputTell = document.querySelector("#job_mobile, #_job_mobile");
-//
-//     if (inputTell && !inputTell.getAttribute('placeholder')) {
-//         inputTell.setAttribute('dir', 'ltr')
-//
-//         window.intlTelInput(inputTell, {
-//             autoInsertDialCode: true,
-//             onlyCountries: wp_sms_intel_tel_input.only_countries,
-//             preferredCountries: wp_sms_intel_tel_input.preferred_countries,
-//             autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
-//             nationalMode: wp_sms_intel_tel_input.national_mode,
-//             utilsScript: wp_sms_intel_tel_input.util_js,
-//             formatOnDisplay: false,
-//             //initialCountry: 'auto'
-//         });
-//     }
-// }
-
 jQuery(document).ready(function () {
     setTimeout(init, 1500);
 });
@@ -70,7 +28,7 @@ function init() {
         if (!inputTells.length) {
             inputTells = document.querySelectorAll("#billing-phone");
         }
-        console.log(inputTells);
+
         if (!inputTells.length) {
             inputTells = document.querySelectorAll(".wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone, #billing-phone, #wp-sms-input-mobile, .user-mobile-wrap #mobile");
         }
