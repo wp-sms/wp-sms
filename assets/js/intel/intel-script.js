f9242ddd823c658fd90deb610a792c25076b6b40
Nora <mwdrad@gmail.com>
2024-05-26 11:11:56 +0330

    Fix intlTelInput country code

--- assets/js/intel/intel-script.js
+++ assets/js/intel/intel-script.js
@@ -1,29 +1,33 @@
 jQuery(document).ready(function () {
-    setTimeout(() => init(), 500);    
+    setTimeout(() => init(), 500);
 });
 
 function init() {
     var inputTells = document.querySelectorAll(".wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone, #billing-phone, #wp-sms-input-mobile, .user-mobile-wrap #mobile");
 
-    for (var i = 0; i < inputTells.length; i++) {
-        if (inputTells[i] && inputTells[i].nodeName == 'INPUT') {
-            const body = document.body;
-            const direction = body.classList.contains('rtl') ? 'rtl' : 'ltr';
-            inputTells[i].setAttribute('dir', direction)
-            window.intlTelInput(inputTells[i], {
-                autoInsertDialCode: true,
-                allowDropdown:true,
-                strictMode:false,
-                autoPlaceholder:"aggressive",
-                onlyCountries: wp_sms_intel_tel_input.only_countries,
-                preferredCountries: wp_sms_intel_tel_input.preferred_countries,
-                //autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
-                nationalMode: false,
-                useFullscreenPopup:false,
-                utilsScript: wp_sms_intel_tel_input.util_js,
-                formatOnDisplay: false,
-                // initialCountry: 'auto'
-            });
+    if (inputTells && inputTells.length) {
+        for (var i = 0; i < inputTells.length; i++) {
+            var parentElement = inputTells[i].parentNode;
+            if (inputTells[i] && inputTells[i].nodeName == 'INPUT') {
+                const body = document.body;
+                const direction = body.classList.contains('rtl') ? 'rtl' : 'ltr';
+                inputTells[i].setAttribute('dir', direction);
+
+                window.intlTelInput(inputTells[i], {
+                    separateDialCode: true,
+                    allowDropdown: true,
+                    strictMode: true,
+                    autoPlaceholder: "aggressive",
+                    onlyCountries: wp_sms_intel_tel_input.only_countries,
+                    countryOrder: wp_sms_intel_tel_input.preferred_countries,
+                    //autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
+                    nationalMode: false,
+                    useFullscreenPopup: false,
+                    utilsScript: wp_sms_intel_tel_input.util_js,
+                    formatOnDisplay: false,
+                    initialCountry: 'us'
+                });
+            }
         }
     }
 
@@ -36,10 +40,10 @@ function init() {
 
         window.intlTelInput(inputTell, {
             autoInsertDialCode: true,
-            autoPlaceholder:"aggressive",
-            allowDropdown:true,
-            strictMode:false,
-            useFullscreenPopup:false,
+            autoPlaceholder: "aggressive",
+            allowDropdown: true,
+            strictMode: false,
+            useFullscreenPopup: false,
             onlyCountries: wp_sms_intel_tel_input.only_countries,
             preferredCountries: wp_sms_intel_tel_input.preferred_countries,
             autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
