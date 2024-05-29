251d7b5f8b51593449a7ca33a4ae86b8207a529a
Nora <mwdrad@gmail.com>
2024-05-29 19:32:42 +0330

    Minor improvement

--- assets/js/intel/intel-script.js
+++ assets/js/intel/intel-script.js
@@ -1,123 +1,89 @@
 jQuery(document).ready(function () {
+
     setTimeout(init, 1500);
 });
 
-const body = document.body;
-const direction = body.classList.contains('rtl') ? 'rtl' : 'ltr';
 
 function init() {
-    function initializeInput(input) {
-        if (input && input.nodeName === 'INPUT') {
-             input.setAttribute('dir', direction);
+    const body = document.body;
+    const direction = body.classList.contains('rtl') ? 'rtl' : 'ltr';
 
-            let iti = window.intlTelInput(input, {
-                separateDialCode: false,
-                allowDropdown: true,
-                strictMode: true,
-                onlyCountries: wp_sms_intel_tel_input.only_countries,
-                countryOrder: wp_sms_intel_tel_input.preferred_countries,
-                nationalMode: true,
-                useFullscreenPopup: false,
-                utilsScript: wp_sms_intel_tel_input.util_js,
-                formatOnDisplay: false,
-                initialCountry: 'us'
-            });
+    // Initialize input fields with intlTelInput
 
-            let currentDialCode = iti.getSelectedCountryData().dialCode;
-
-            function updateInputWithCountryCode(input, newDialCode) {
-                let value = input.value;
-
-                // Remove all non-digit characters for comparison
-                let valueDigitsOnly = value.replace(/\D/g, '');
-
-                // Check if the input already contains the current country dial code
-                if (!valueDigitsOnly.startsWith(newDialCode)) {
-                    // Create regex pattern to match old dial code at the beginning
-                    let oldDialCodePattern = new RegExp(`^(\\+?${currentDialCode})`);
-                    let valueWithoutOldCode = value.replace(oldDialCodePattern, '').trim();
-
-                    // Set the new value with the new dial code
-                    input.value = (`+${newDialCode} ${valueWithoutOldCode}`).replace(/[-\s]/g, '');
-
-                    // Update current dial code
-                    currentDialCode = newDialCode;
-
-                    // Simulate the change event
-                    simulateChangeEvent(input);
-                }
+    function initializeInputs(inputTells) {
+        for (var i = 0; i < inputTells.length; i++) {
+              if (inputTells[i] && inputTells[i].nodeName === 'INPUT') {
+                 inputTells[i].setAttribute('dir', direction);
+                window.intlTelInput(inputTells[i], {
+                    separateDialCode: false,
+                    allowDropdown: true,
+                    strictMode: true,
+                    onlyCountries: wp_sms_intel_tel_input.only_countries,
+                    countryOrder: wp_sms_intel_tel_input.preferred_countries,
+                     nationalMode: true,
+                    useFullscreenPopup: false,
+                    dropdownContainer: body.classList.contains('rtl') ? null : body,
+                    utilsScript: wp_sms_intel_tel_input.util_js,
+                    hiddenInput: () => ({ phone: inputTells[i].name}),
+                    formatOnDisplay: false,
+                    initialCountry: wp_sms_intel_tel_input.only_countries.length > 0 ? wp_sms_intel_tel_input.only_countries[0] :  'us'
+                 });
+                  function setDefaultCode(item){
+                      let iti = intlTelInput.getInstance(item);
+                      if(item.value==''){
+                          let country=iti.getSelectedCountryData();
+                          item.value = '+'+country.dialCode;
+                      }else{
+                          if(iti.getNumber()){
+                               item.value=iti.getNumber().replace(/[-\s]/g, '')
+                          }else{
+                              item.value=item.value.replace(/[-\s]/g, '')
+                          }
+                      }
+                  }
+                  setDefaultCode(inputTells[i]);
+                  inputTells[i].addEventListener('blur', function() {
+                      setDefaultCode(this)
+                  });
             }
 
-            function simulateChangeEvent(input) {
-                const event = new Event('change', {
-                    bubbles: true,
-                    cancelable: true
-                });
-                input.dispatchEvent(event);
-            }
-
-            input.addEventListener('blur', function () {
-                input.value=(input.value).replace(/[-\s]/g, '')
-                updateInputWithCountryCode(this, iti.getSelectedCountryData().dialCode);
-            });
-
-            input.addEventListener('countrychange', function () {
-                let newDialCode = iti.getSelectedCountryData().dialCode;
-                input.value = `+${newDialCode}`; // Set the input value to the new country code
-                updateInputWithCountryCode(this, newDialCode);
-            });
-        }
+         }
     }
 
+    // Check and initialize the main input fields
     function checkAndInitializeInputs() {
-        const primaryInput = document.querySelector('#billing-wpsms\\/mobile');
+        const primaryInput = document.querySelectorAll('#billing-wpsms\\/mobile');
         const isWooCommerceCheckoubBlock = document.querySelector('.wc-block-checkout ');
         if (isWooCommerceCheckoubBlock) {
             return null;
         }
-        if (primaryInput) {
-            initializeInput(primaryInput);
-        } else {
-            const inputTells = document.querySelectorAll('.wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone, #billing-phone, #wp-sms-input-mobile, .user-mobile-wrap #mobile');
-            if (inputTells.length) {
-                inputTells.forEach(input => {
-                    initializeInput(input);
-                });
-            }
+        if (!primaryInput.length) {
+            inputTells = document.querySelectorAll(".wp-sms-input-mobile, .wp-sms-input-mobile #billing_phone,#billing-phone , #wp-sms-input-mobile, .user-mobile-wrap #mobile");
         }
+        initializeInputs(inputTells);
     }
 
     checkAndInitializeInputs();
 
-    const shippingCheckbox = document.querySelector('#shipping-fields .wc-block-checkout__use-address-for-billing input');
-    if (shippingCheckbox) {
-        shippingCheckbox.addEventListener('change', function () {
-            if (document.querySelector('#billing-fields')) {
-                setTimeout(checkAndInitializeInputs, 500);  // Reinitialize to check and initialize inputs again if needed
-            }
-        });
-    }
-
-
-
-
+    // Additional specific input field initialization
     var inputTell = document.querySelector("#job_mobile, #_job_mobile");
     if (inputTell && !inputTell.getAttribute('placeholder')) {
-
         inputTell.setAttribute('dir', direction)
         window.intlTelInput(inputTell, {
-            separateDialCode: false,
+            autoInsertDialCode: true,
+            autoPlaceholder: "aggressive",
             allowDropdown: true,
             strictMode: true,
             useFullscreenPopup: false,
+            dropdownContainer: body.classList.contains('rtl') ? null : body,
             onlyCountries: wp_sms_intel_tel_input.only_countries,
             countryOrder: wp_sms_intel_tel_input.preferred_countries,
             autoHideDialCode: wp_sms_intel_tel_input.auto_hide,
             nationalMode: wp_sms_intel_tel_input.national_mode,
             utilsScript: wp_sms_intel_tel_input.util_js,
             formatOnDisplay: false,
-            initialCountry: 'us'
-        });
+            initialCountry: wp_sms_intel_tel_input.only_countries.length > 0 ? wp_sms_intel_tel_input.only_countries[0] :  'us'
+         });
         function setDefaultCode(item){
             let iti = intlTelInput.getInstance(item);
             if(item.value==''){
@@ -125,7 +91,7 @@ function init() {
                 item.value = '+'+country.dialCode;
             }else{
                 if(iti.getNumber()){
-                    item.value=iti.getNumber().replace(/[-\s]/g, '')
+                     item.value=iti.getNumber().replace(/[-\s]/g, '')
                 }else{
                     item.value=item.value.replace(/[-\s]/g, '')
                 }
@@ -136,4 +102,13 @@ function init() {
         });
     }
 
+    // Handle the change event for the checkbox
+    const shippingCheckbox = document.querySelector('#shipping-fields .wc-block-checkout__use-address-for-billing input');
+    if (shippingCheckbox) {
+        shippingCheckbox.addEventListener('change', function() {
+            if (document.querySelector('#billing-fields')) {
+                setTimeout(checkAndInitializeInputs, 500);
+            }
+        });
+    }
 }
