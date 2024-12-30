f2fb6e6c77801e467a5aca752a2d463ba927ba32
Erfan Kargosha <ekargosha88@gmail.com>
2024-12-30 16:02:47 +0330

    adding info comment

--- includes/class-wpsms-notifications.php
+++ includes/class-wpsms-notifications.php
@@ -164,6 +164,7 @@ class Notifications
             $userMobileNumberFromRequest = sanitize_text_field($_REQUEST['mobile']);
             $receiver                    = [$userMobileNumberFromRequest];
         } elseif (isset($_REQUEST['phone_number'])) {
+            // used for sending SMS to user after registration on LOGIN page.
             $userMobileNumberFromRequest = sanitize_text_field($_REQUEST['phone_number']);
             $receiver                    = [$userMobileNumberFromRequest];
         }
