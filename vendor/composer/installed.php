7f6da58bce536eaa50bea56a0fca78773f8bc013
Erfan Kargosha <ekargosha88@gmail.com>
2025-01-20 10:58:10 +0100

    updating composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'ad11cb935d1f9c417f707b2577068cf4e762336c',
+        'reference' => 'c8a88542b75a775e9f5c93ba84155cefec98fd2c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'ad11cb935d1f9c417f707b2577068cf4e762336c',
+            'reference' => 'c8a88542b75a775e9f5c93ba84155cefec98fd2c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
