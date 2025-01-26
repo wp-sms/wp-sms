13c86256d70f8ebf3f0b5fe024c0d04e2b97a0df
Erfan Kargosha <ekargosha88@gmail.com>
2025-01-26 14:11:25 +0330

    fix download page items not loading

--- views/pages/license-manager/downloads.php
+++ views/pages/license-manager/downloads.php
@@ -11,19 +11,20 @@ use WP_SMS\Admin\LicenseManagement\Plugin\PluginDecorator;
     <div class="wpsms-postbox-addon__step">
         <div class="wpsms-addon__step__info">
             <span class="wpsms-addon__step__image wpsms-addon__step__image--checked"></span>
-            <h2 class="wpsms-addon__step__title"><?php esc_html_e('You\'re All Set! Your License is Successfully Activated!', 'wp-statistics'); ?></h2>
-            <p class="wpsms-addon__step__desc"><?php esc_html_e('Choose the add-ons you want to install. You can modify your selection later.', 'wp-statistics'); ?></p>
+            <h2 class="wpsms-addon__step__title"><?php esc_html_e('You\'re All Set! Your License is Successfully Activated!', 'wp-sms'); ?></h2>
+            <p class="wpsms-addon__step__desc"><?php esc_html_e('Choose the add-ons you want to install. You can modify your selection later.', 'wp-sms'); ?></p>
         </div>
         <div class="wpsms-addon__step__download">
             <div class="wpsms-addon__download__title">
                 <h3>
-                    <?php esc_html_e('Select Your Add-Ons', 'wp-statistics'); ?>
+                    <?php esc_html_e('Select Your Add-Ons', 'wp-sms'); ?>
                 </h3>
-                <a class="wpsms-addon__download_select-all js-wpsms-addon-select-all <?php echo empty($data['display_select_all']) ? 'wpsms-hide' : ''; ?>"><?php esc_html_e('Select All', 'wp-statistics'); ?></a>
+                <a class="wpsms-addon__download_select-all js-wpsms-addon-select-all <?php echo empty($data['display_select_all']) ? 'wpsms-hide' : ''; ?>"><?php esc_html_e('Select All', 'wp-sms'); ?></a>
             </div>
             <div class="wpsms-addon__download__items">
                 <?php
                 if (!empty($data['licensed_addons'])) {
+
                     /** @var PluginDecorator $addOn */
                     foreach ($data['licensed_addons'] as $addOn) {
                         View::load('components/addon-download-card', ['addOn' => $addOn, 'included' => true]);
@@ -40,9 +41,9 @@ use WP_SMS\Admin\LicenseManagement\Plugin\PluginDecorator;
             </div>
         </div>
         <div class="wpsms-addon__step__action">
-            <a href="<?php echo esc_url(MenuUtil::getAdminUrl('plugins', ['tab' => 'add-license'])); ?>" class="wpsms-addon__step__back"><?php esc_html_e('Back', 'wp-statistics'); ?></a>
+            <a href="<?php echo esc_url(MenuUtil::getAdminUrl('plugins', ['tab' => 'add-license'])); ?>" class="wpsms-addon__step__back"><?php esc_html_e('Back', 'wp-sms'); ?></a>
             <a class="wpsms-postbox-addon-button js-addon-download-button disabled">
-                <?php esc_html_e('Download & Install Selected Add-Ons', 'wp-statistics'); ?>
+                <?php esc_html_e('Download & Install Selected Add-Ons', 'wp-sms'); ?>
             </a>
         </div>
     </div>
