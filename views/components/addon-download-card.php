6ad11e9c462a9d462d817c85707ba172c309c6fb
mrmolaei <mrmolaei.ir@gmail.com>
2025-01-27 12:31:13 +0330

    Add add-ons images & small changes

--- views/components/addon-download-card.php
+++ views/components/addon-download-card.php
@@ -6,38 +6,38 @@ if (!defined('ABSPATH') || empty($addOn)) {
 }
 
 ?>
-<div class="wps-addon__download__item <?php echo !$included ? 'wps-addon__download__item--disabled' : ''; ?>">
-    <div class="wps-addon__download__item--info">
-        <div class="wps-addon__download__item--info__img">
+<div class="wpsms-addon__download__item <?php echo !$included ? 'wpsms-addon__download__item--disabled' : ''; ?>">
+    <div class="wpsms-addon__download__item--info">
+        <div class="wpsms-addon__download__item--info__img">
             <img src="<?php echo esc_url($addOn->getIcon()); ?>" alt="<?php echo esc_attr($addOn->getName()); ?>">
         </div>
-        <div class="wps-addon__download__item--info__text">
-            <div class="wps-addon__download__item--info__title">
+        <div class="wpsms-addon__download__item--info__text">
+            <div class="wpsms-addon__download__item--info__title">
                 <?php echo esc_html($addOn->getName()); ?>
                 <?php if (!empty($addOn->getProductUrl())) : ?>
-                    <a target="_blank" href="<?php echo esc_html($addOn->getProductUrl()); ?>?utm_source=wp-sms&utm_medium=link&utm_campaign=dp" class="wps-postbox-addon__read-more" title="<?php esc_html_e('Learn More', 'wp-sms'); ?>">
+                    <a target="_blank" href="<?php echo esc_html($addOn->getProductUrl()); ?>?utm_source=wp-sms&utm_medium=link&utm_campaign=dp" class="wpsms-postbox-addon__read-more" title="<?php esc_html_e('Learn More', 'wp-sms'); ?>">
                         <?php esc_html_e('Learn More', 'wp-sms'); ?>
                     </a>
                 <?php endif; ?>
 
                 <?php if ($included && $addOn->isUpdateAvailable()) : ?>
-                    <span class="wps-postbox-addon__label wps-postbox-addon__label--updated"><?php esc_html_e('Update Available', 'wp-sms'); ?></span>
+                    <span class="wpsms-postbox-addon__label wpsms-postbox-addon__label--updated"><?php esc_html_e('Update Available', 'wp-sms'); ?></span>
                 <?php endif; ?>
             </div>
-            <p class="wps-addon__download__item--info__desc">
+            <p class="wpsms-addon__download__item--info__desc">
                 <?php echo wp_kses($addOn->getShortDescription(), 'data'); ?>
             </p>
         </div>
     </div>
-    <div class="wps-addon__download__item--select">
+    <div class="wpsms-addon__download__item--select">
         <?php if ($included && $addOn->isInstalled()) : ?>
-            <span class="wps-postbox-addon__status wps-postbox-addon__status--installed "><?php esc_html_e('Already installed', 'wp-sms'); ?></span>
+            <span class="wpsms-postbox-addon__status wpsms-postbox-addon__status--installed "><?php esc_html_e('Already installed', 'wp-sms'); ?></span>
         <?php elseif (!$included) : ?>
-            <span class="wps-postbox-addon__status wps-postbox-addon__status--primary "><?php esc_html_e('Not included', 'wp-sms'); ?></span>
+            <span class="wpsms-postbox-addon__status wpsms-postbox-addon__status--primary "><?php esc_html_e('Not included', 'wp-sms'); ?></span>
         <?php endif; ?>
 
         <?php if ($included && (!$addOn->isInstalled() || $addOn->isUpdateAvailable())) : ?>
-            <span> <input type="checkbox" class="js-wps-addon-check-box" name="addon-select" data-slug="<?php echo esc_attr($addOn->getSlug()); ?>"></span>
+            <span> <input type="checkbox" class="js-wpsms-addon-check-box" name="addon-select" data-slug="<?php echo esc_attr($addOn->getSlug()); ?>"></span>
         <?php endif; ?>
     </div>
 </div>
