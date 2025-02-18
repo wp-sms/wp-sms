9b6eeea8e0d05faf056c0ffb2595757864a2e201
Erfan Kargosha <ekargosha88@gmail.com>
2025-02-18 14:00:32 +0330

    fix url generation and make steps navigation dynamic

--- views/templates/layout/onboarding/header.php
+++ views/templates/layout/onboarding/header.php
@@ -8,13 +8,13 @@
             <div class="c-header">
                 <nav class="c-header_navigation">
                     <ul class="s-nav s-nav--steps">
-                        <li class="<?php echo $current == 'getting-started' ? 'is-active' : '' ?>"><span></span><a href="/" title="Getting Started">Getting Started</a></li>
-                        <li class="<?php echo $current == 'sms-gateway' ? 'is-active' : '' ?>"><span></span><a href="/step2-sms-gateway.html" title="SMS gateway">SMS gateway</a></li>
-                        <li class="<?php echo $current == 'configuration' ? 'is-active' : '' ?>"><span></span><a href="/step3-configuration-result.html" title="Configuration">Configuration</a></li>
-                        <li class="<?php echo $current == 'test-setup' ? 'is-active' : '' ?>"><span></span><a href="/step4-industry.html" title="Test Your Setup">Test Your Setup</a></li>
-                        <li class="<?php echo $current == 'update-all-in-one' ? 'is-active' : '' ?>"><span></span><a href="/step5-wp-sms-pro.html" title="WP-SMS Pro">WP-SMS Pro</a></li>
-                        <li class="<?php echo $current == 'addons' ? 'is-active' : '' ?>"><span></span><a href="/step6-addons.html" title="Add-ons">Add-ons</a></li>
-                        <li class="<?php echo $current == 'ready' ? 'is-active' : '' ?>"><span></span><a href="/step7-ready.html" title="Ready">Ready</a></li>
+                        <?php
+
+                        foreach ($steps as $key => $step) {
+                            $isActive = ($key < $index) ? 'is-active' : '';
+                            echo '<li class="' . $isActive . '"><span></span><a href="' . $step['url'] . '" title="' . $step['title'] . '">' . $step['title'] . '</a></li>';
+                        }
+                        ?>
                     </ul>
                 </nav>
             </div>
