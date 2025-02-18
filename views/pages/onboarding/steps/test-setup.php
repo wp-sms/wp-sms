304d2445c4542f05c8c8000f2189c7f30d9f7e49
Nora <mwdrad@gmail.com>
2025-02-18 15:53:04 +0330

    pref : Add alerts to step 4

--- views/pages/onboarding/steps/test-setup.php
+++ views/pages/onboarding/steps/test-setup.php
@@ -1,17 +1,30 @@
 <div class="c-section__title">
-    <span class="c-section__step">Step <?php echo $index ?> of 7</span>
-    <h1 class=" u-m-0">Test Your Setup</h1>
+    <span class="c-section__step"><?php echo esc_html(sprintf(__('Step %d of 7', 'wp-sms'), $index)); ?></span>
+    <h1 class=" u-m-0"><?php esc_html_e('Test Your Setup', 'wp-sms'); ?></h1>
     <p class="u-m-0">
-        Send a test SMS to the administrator's phone number to confirm everything is working as it should. </p>
+        <?php esc_html_e('Send a test SMS to the administrator\'s phone number to confirm everything is working as it should. ', 'wp-sms'); ?>
+    </p>
 </div>
 <form method="post" action="<?php echo $ctas['next']['url'] ?>">
     <div class="c-section__test">
-        <p class="c-section__test--title">A test message will be sent to the phone number you provided earlier. Please
-            check your device for the message.</p>
-        <p class="c-section__test--title">Did you receive the test SMS?</p>
-        <p class="c-section__test--description">If you've received the test SMS, clicking 'Yes, I received it!' will
-            confirm your setup is correct and take you to the next step. If not, select 'No, I didn't receive it.' for
-            troubleshooting options.</p>
+        <div class="wpsms-admin-alert wpsms-admin-alert--success">
+            <div class="wpsms-admin-alert--content">
+                <p>
+                    <?php esc_html_e('A test message will be sent to the phone number you provided earlier. Please
+                check your device for the message.', 'wp-sms'); ?>
+                </p>
+            </div>
+        </div>
+        <div class="wpsms-admin-alert wpsms-admin-alert--info">
+            <div class="wpsms-admin-alert--content">
+                <h2><?php esc_html_e('Did you receive the test SMS?', 'wp-sms'); ?></h2>
+                <p>
+                    <?php esc_html_e('If you\'ve received the test SMS, clicking \'Yes, I received it!\' will
+            confirm your setup is correct and take you to the next step. If not, select \'No, I didn\'t receive it.\' for
+            troubleshooting options.', 'wp-sms'); ?>
+                </p>
+            </div>
+        </div>
     </div>
     <div class="c-form__footer u-content-sp u-align-center">
         <a class="c-form__footer--last-step" href="<?php echo $ctas['back']['url'] ?>"><?php echo $ctas['back']['text'] ?></a>
--- views/templates/layout/onboarding/footer.php
+++ views/templates/layout/onboarding/footer.php
@@ -1,6 +1,6 @@
 </div>
 </section>
 <section class="c-section--nextstep u-text-center">
-    <a class="c-link" href="#" title="Skip setup WP SMS">Skip setup WP SMS</a>
+    <a class="c-link" href="#" title="<?php esc_html_e('Skip this step', 'wp-sms'); ?>"><?php esc_html_e('Skip this step', 'wp-sms'); ?></a>
 </section>
 </div>
