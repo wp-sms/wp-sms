ccb8631afd869060fdcf2f6af892089c0386e9a2
Erfan Kargosha <ekargosha88@gmail.com>
2025-03-27 11:24:11 +0330

    use wp_remote_request to support all request methods

--- src/Components/RemoteRequest.php
+++ src/Components/RemoteRequest.php
@@ -35,11 +35,20 @@ class RemoteRequest
         $params = apply_filters('wp_sms_request_params', $params);
 
         /**
-         * Prepare the arguments
+         * Add Basic Authentication if credentials are defined
+         */
+        if (defined('WP_SMS_API_USERNAME') && defined('WP_SMS_API_PASSWORD')) {
+            $basic_auth = 'Basic ' . base64_encode(WP_SMS_API_USERNAME . ':' . WP_SMS_API_PASSWORD);
+            $params['headers']['Authorization'] = $basic_auth;
+        }
+
+        /**
+         * Prepare the arguments including the method
          */
         $this->parsedParams = wp_parse_args($params, [
             'timeout' => 10,
-            'headers' => array()
+            'headers' => [],
+            'method' => strtoupper($method)
         ]);
 
         /**
@@ -105,37 +114,24 @@ class RemoteRequest
             }
         }
 
-        $response = null;
+        // Execute the request using wp_remote_request
+        $this->response = wp_remote_request($this->requestUrl, $this->parsedParams);
 
-        switch ($this->method) {
-            case 'GET':
-                $response = wp_remote_get($this->requestUrl, $this->parsedParams);
-                break;
-
-            case 'POST':
-                $response = wp_remote_post($this->requestUrl, $this->parsedParams);
-                break;
-
-            default:
-                throw new Exception(esc_html(sprintf(__('Unsupported HTTP method: %s', 'wp-sms'), $this->method)));
+        if (is_wp_error($this->response)) {
+            throw new Exception(esc_html($this->response->get_error_message()));
         }
 
-        if (is_wp_error($response)) {
-            throw new Exception(esc_html($response->get_error_message()));
-        }
+        $this->responseCode = wp_remote_retrieve_response_code($this->response);
+        $this->responseBody = wp_remote_retrieve_body($this->response);
 
-        $this->responseCode = wp_remote_retrieve_response_code($response);
-        $this->responseBody = wp_remote_retrieve_body($response);
-
-        if ($throwFailedHttpCodeResponse) {
-            if (!in_array($this->responseCode, [200, 201, 202], true)) {
-                if (Helper::isJson($this->responseBody)) {
-                    $responseBody = json_decode($this->responseBody, true);
-                }
-
-                // translators: %s: Response message
-                throw new Exception(sprintf(esc_html__('Failed to get success response, %s', 'wp-sms'), esc_html(print_r($responseBody, 1))));
+        if ($throwFailedHttpCodeResponse && !$this->isRequestSuccessful()) {
+            $responseBody = $this->responseBody;
+            if (Helper::isJson($responseBody)) {
+                $responseBody = json_decode($responseBody, true);
             }
+
+            // translators: %s: Response message
+            throw new Exception(sprintf(esc_html__('Failed to get success response, %s', 'wp-sms'), esc_html(print_r($responseBody, 1))));
         }
 
         $responseJson = json_decode($this->responseBody);
