cac088f5fccf3a0321f99519022684183c363c6e
Erfan Kargosha <ekargosha88@gmail.com>
2025-04-09 12:10:49 +0330

    fix notice handling

--- src/Admin/LicenseManagement/LicenseMigration.php
+++ src/Admin/LicenseManagement/LicenseMigration.php
@@ -3,7 +3,7 @@
 namespace WP_SMS\Admin\LicenseManagement;
 
 use WP_SMS\Utils\OptionUtil;
-use WP_SMS\Admin\NoticeHandler\Notice;
+use WP_SMS\Notice\NoticeManager;
 
 class LicenseMigration
 {
@@ -74,7 +74,7 @@ class LicenseMigration
         // Modify keys to remove "license_" prefix and "_key" suffix
         $modifiedLicenses = [];
         foreach ($licenseFilteredArray as $key => $value) {
-            $newKey = preg_replace('/^license_(.*)_key$/', '$1', $key);
+            $newKey                    = preg_replace('/^license_(.*)_key$/', '$1', $key);
             $modifiedLicenses[$newKey] = $value;
         }
 
@@ -106,7 +106,7 @@ class LicenseMigration
     /**
      * Attempts to migrate a single license.
      *
-     * @param string $addonSlug  The slug of the add-on.
+     * @param string $addonSlug The slug of the add-on.
      * @param string $licenseKey The license key associated with the add-on.
      *
      * @return bool True if the license migration was successful, false otherwise.
@@ -116,12 +116,8 @@ class LicenseMigration
         try {
             $this->apiCommunicator->validateLicense($licenseKey, $addonSlug);
         } catch (\Exception $e) {
-            Notice::addNotice(
-                sprintf(__('Failed to migrate license for %s: %s', 'wp-sms'), $addonSlug, $e->getMessage()),
-                'license_migration',
-                'error'
-            );
-
+            $notice = NoticeManager::getInstance();
+            $notice->registerNotice('license_migration', sprintf(__('Failed to migrate license for %s: %s', 'wp-sms'), $addonSlug, $e->getMessage()));
             return false;
         }
 
