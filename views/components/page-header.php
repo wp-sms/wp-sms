64d6e4e4d7ce98e4467fc7acb4c8a34a0e853659
Nora <mwdrad@gmail.com>
2025-06-10 14:19:35 +0330

    revert : Remove unnecessary code

--- views/components/page-header.php
+++ views/components/page-header.php
@@ -2,15 +2,8 @@
 
 ?>
 <div class="wpsms-wrap__top">
-    <?php if (isset($backUrl, $backTitle)): ?>
-        <a href="<?php echo esc_url($backUrl) ?>" title="<?php echo esc_html($backTitle) ?>" class="wpsms-previous-url"><?php echo esc_html($backTitle) ?></a>
-    <?php endif ?>
-    
     <?php if (isset($title)): ?>
         <h2 class="wpsms_title <?php echo isset($install_addon_btn_txt) ? 'wpsms_plugins_page-title' : '' ?>">
-            <?php if (isset($flagImage)): ?>
-                <img class="wpsms-flag" src="<?php echo esc_url($flagImage) ?>" alt="<?php echo esc_attr($title) ?>">
-             <?php endif ?>
             <?php echo(isset($title) ? esc_attr($title) : (function_exists('get_admin_page_title') ? get_admin_page_title() : '')); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped	 ?>
             <?php if (!empty($tooltip)) : ?>
                 <span class="wpsms-tooltip" title="<?php echo esc_attr($tooltip); ?>"><i class="wpsms-tooltip-icon info"></i></span>
@@ -25,46 +18,6 @@
     <?php endif ?>
 
     <?php do_action('wp_sms_after_admin_page_title'); ?>
-
-    <?php if (isset($Datepicker)): ?>
-        <form class="wpsms-search-date wpsms-today-datepicker" method="get">
-            <div>
-                <input type="hidden" name="page" value="<?php echo esc_attr($pageName); ?>">
-                <input class="wpsms-search-date__input wpsms-js-calendar-field" id="search-date-input" type="text" size="18" name="day" data-wpsms-date-picker="day" readonly value="<?php echo esc_attr($day); ?>" autocomplete="off" placeholder="YYYY-MM-DD" required>
-            </div>
-        </form>
-    <?php endif ?>
-
-    <?php if (isset($hasDateRang) || isset($filters) || isset($searchBoxTitle) || isset($filter)): ?>
-        <div class="wpsms-head-filters">
-            <?php
-            if (!empty($hasDateRang)) {
-                include 'date.range.php';
-            }
-
-            if (isset($filter) and isset($filter['code'])) {
-                echo $filter['code']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                ?>
-                <div class="wp-clearfix"></div>
-                <?php
-            }
-
-            if (!empty($filters)) {
-                foreach ($filters as $filter) {
-                    require_once "filters/$filter-filter.php";
-                }
-            }
-
-            if (isset($searchBoxTitle)): ?>
-                <div class="wpsms-filter-visitor wpsms-head-filters__item loading">
-                    <div class="wpsms-dropdown">
-                        <label for="wpsms-visitor-filter" class="selectedItemLabel"><?php echo esc_attr($searchBoxTitle); ?></label>
-                        <select id="wpsms-visitor-filter" class="wpsms-select2" data-type-show="select2"></select>
-                    </div>
-                </div>
-            <?php endif ?>
-        </div>
-    <?php endif ?>
 </div>
 <div class="wpsms-wrap__main">
     <div class="wp-header-end"></div>
