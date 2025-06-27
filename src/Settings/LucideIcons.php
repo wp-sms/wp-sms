f9065579114395ea917eea2e2debd6ce0af625dc
Amir Pirmoradian <piramir77@gmail.com>
2025-06-27 22:48:14 +0330

    refactor(settings): update LucideIcons to use string constants for improved type safety

--- src/Settings/LucideIcons.php
+++ src/Settings/LucideIcons.php
@@ -9,40 +9,41 @@ namespace WP_SMS\Settings;
 class LucideIcons
 {
     // Core Settings Icons
-    public const SETTINGS = 'Settings';
-    public const SETTINGS_2 = 'Settings2';
-    public const COG = 'Cog';
-    public const MESSAGE_SQUARE = 'MessageSquare';
-    public const SEND = 'Send';
-    public const ZAP = 'Zap';
-    public const STAR = 'Star';
-    public const BELL = 'Bell';
-    public const MAIL = 'Mail';
-    public const USERS = 'Users';
-    public const MOUSE_POINTER = 'MousePointer';
-    public const MESSAGE_CIRCLE = 'MessageCircle';
-    public const NEWSPAPER = 'Newspaper';
+    public const string SETTINGS = 'Settings';
+    public const string SETTINGS_2 = 'Settings2';
+    public const string COG = 'Cog';
+    public const string MESSAGE_SQUARE = 'MessageSquare';
+    public const string SEND = 'Send';
+    public const string ZAP = 'Zap';
+    public const string STAR = 'Star';
+    public const string BELL = 'Bell';
+    public const string MAIL = 'Mail';
+    public const string USERS = 'Users';
+    public const string MOUSE_POINTER = 'MousePointer';
+    public const string MESSAGE_CIRCLE = 'MessageCircle';
+    public const string NEWSPAPER = 'Newspaper';
     
     // Integration Icons
-    public const SHIELD = 'Shield';
-    public const LOCK = 'Lock';
-    public const SHOPPING_CART = 'ShoppingCart';
-    public const USER_CHECK = 'UserCheck';
-    public const GRADUATION_CAP = 'GraduationCap';
-    public const CALENDAR = 'Calendar';
-    public const BAR_CHART_3 = 'BarChart3';
+    public const string SHIELD = 'Shield';
+    public const string LOCK = 'Lock';
+    public const string SHOPPING_CART = 'ShoppingCart';
+    public const string USER_CHECK = 'UserCheck';
+    public const string GRADUATION_CAP = 'GraduationCap';
+    public const string CALENDAR = 'Calendar';
+    public const string BAR_CHART_3 = 'BarChart3';
     
     // Utility Icons
-    public const HELP_CIRCLE = 'HelpCircle';
-    public const EXTERNAL_LINK = 'ExternalLink';
-    public const ALERT_TRIANGLE = 'AlertTriangle';
-    public const INFO = 'Info';
-    public const SPARKLES = 'Sparkles';
-    public const BEAKER = 'Beaker';
-    public const CROWN = 'Crown';
-    public const TEST_TUBE = 'TestTube';
-    public const CLOCK = 'Clock';
-    public const BADGE_CHECK = 'BadgeCheck';
+    public const string HELP_CIRCLE = 'HelpCircle';
+    public const string EXTERNAL_LINK = 'ExternalLink';
+    public const string ALERT_TRIANGLE = 'AlertTriangle';
+    public const string INFO = 'Info';
+    public const string SPARKLES = 'Sparkles';
+    public const string BEAKER = 'Beaker';
+    public const string CROWN = 'Crown';
+    public const string TEST_TUBE = 'TestTube';
+    public const string CLOCK = 'Clock';
+    public const string BADGE_CHECK = 'BadgeCheck';
+    public const string FILE_TEXT = 'FileText';
     
     /**
      * Get all available icons
