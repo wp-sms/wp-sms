b53cd9fccd4cd146dc0f8fa1739c001fc6defb78
Amir Pirmoradian <piramir77@gmail.com>
2025-07-05 12:22:18 +0330

    feat(settings): add Option class for managing plugin options
    - Introduced the Option class to handle plugin settings, including methods for retrieving, adding, and updating options.
    - Implemented functionality to differentiate between standard and Pro settings.
    - Added placeholders for future schema validation integration.

---
+++ src/Settings/Option.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WP_SMS\Settings;
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+class Option
+{
+    /**
+     * Get the whole Plugin Options
+     *
+     * @param string $setting_name
+     * @param bool $pro
+     *
+     * @return mixed|void
+     */
+    public static function getOptions($pro = false, $setting_name = 'wpsms_settings')
+    {
+        if ($pro) {
+            $setting_name = 'wps_pp_settings';
+        }
+
+        // TODO: Integrate schema/validator here for validation
+        return get_option($setting_name, array());
+    }
+
+    /**
+     * Get the only Option that we want
+     *
+     * @param $option_name
+     * @param bool $pro
+     *
+     * @return string
+     */
+    public static function getOption($option_name, $pro = false)
+    {
+        $options = self::getOptions($pro);
+
+        // TODO: Integrate schema/validator here for validation
+        return isset($options[$option_name]) ? $options[$option_name] : '';
+    }
+
+    /**
+     * Add an option
+     *
+     * @param $option_name
+     * @param $option_value
+     */
+    public static function addOption($option_name, $option_value)
+    {
+        // TODO: Integrate schema/validator here for validation
+        add_option($option_name, $option_value);
+    }
+
+    /**
+     * Update Option
+     *
+     * @param $key
+     * @param $value
+     * @param bool $pro
+     */
+    public static function updateOption($key, $value, $pro = false)
+    {
+        if ($pro) {
+            $setting_name = 'wps_pp_settings';
+        } else {
+            $setting_name = 'wpsms_settings';
+        }
+
+        $options       = self::getOptions($pro);
+        $options[$key] = $value;
+
+        // TODO: Integrate schema/validator here for validation
+        update_option($setting_name, $options);
+    }
+} 
