afba09e9643d85dbfa4fc6bfa27223ea7d68a691
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-20 12:15:28 +0330

    feat: story book config init

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -57,4 +60,4 @@ export default tseslint.config([
       },
     },
   },
-])
+], storybook.configs["flat/recommended"]);
