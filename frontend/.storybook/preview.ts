6ff4c42e8ccc27a52d203421348613ecc1dc17e8
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-22 21:52:34 +0330

    fix: load css in storybook

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,14 +1,15 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from '@storybook/react-vite';
+import '../src/globals.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
