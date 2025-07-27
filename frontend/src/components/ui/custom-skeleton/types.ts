402aaef8bcd0e2841152f28d94ecc416c9b6dd0b
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-27 23:30:02 +0330

    feat: add Accordion, Button, CustomSkeleton, and SimpleHtmlRenderer components.

---
+++ frontend/src/components/ui/custom-skeleton/types.ts
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from 'react';
+
+export type CustomSkeletonProps = PropsWithChildren<{
+    isLoading?: boolean;
+    className?: string;
+    wrapperClassName?: string;
+}>;
