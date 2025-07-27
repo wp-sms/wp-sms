3f9c00e0377a1cb9a5a4fdbf3e05a4c0603c5adb
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-27 23:30:22 +0330

    feat: enhance settings layout with sidebar navigation, breadcrumb, and dynamic rendering of settings items

--- frontend/src/layouts/settings-layout/index.tsx
+++ frontend/src/layouts/settings-layout/index.tsx
@@ -1,11 +1,65 @@
+import { PanelLeftOpen } from 'lucide-react';
 import { SettingsSidebar } from './sidebar';
 import type { SettingsLayoutProps } from './types';
+import { Button } from '@/components/ui/button';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Link } from 'react-router-dom';
+import { useSidebarStore } from '@/stores/sidebar';
 
 export const SettingsLayout: React.FC<SettingsLayoutProps> = ({ children }) => {
+    const { isOpen } = useSidebarStore();
+
+    console.log({ isOpen });
+
     return (
-        <div className="flex h-full">
+        <div className="wrap flex w-full min-h-screen">
             <SettingsSidebar />
-            <div className="flex-1">{children}</div>
+
+            <div className="flex-1 bg-white">
+                <header className="border-b border-b-border p-3 sticky top-8 bg-white z-10">
+                    <div className="flex items-center gap-x-4">
+                        <Button size="icon" variant="ghost" className="cursor-pointer">
+                            <PanelLeftOpen className="text-foreground/85" size={22} />
+                        </Button>
+
+                        <div className="w-[1px] h-5 bg-border rotate-180" />
+
+                        <Breadcrumb>
+                            <BreadcrumbList className="!list-none !ml-1">
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link to="/">WP SMS</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+
+                                <BreadcrumbSeparator />
+
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link to="/">Settings</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+
+                                <BreadcrumbSeparator />
+
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link to="/">General</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+
+                {children}
+            </div>
         </div>
     );
 };
--- frontend/src/layouts/settings-layout/sidebar/index.tsx
+++ frontend/src/layouts/settings-layout/sidebar/index.tsx
@@ -1,25 +1,82 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useGetSettingSchemaList } from '@/models/settings';
+import { SidebarGroup } from './sidebar-group';
+import { SidebarItem } from './sidebar-item';
+import { SidebarNestedItem } from './sidebar-nested-item';
+import { Settings } from 'lucide-react';
+import { useSidebarStore } from '@/stores/sidebar';
+import clsx from 'clsx';
 
 export const SettingsSidebar: React.FC = () => {
-    const location = useLocation();
+    const { data: settingSchemaList } = useGetSettingSchemaList();
+    const { isOpen } = useSidebarStore();
 
     return (
-        <div className="w-64  p-6 border-r">
-            <h2 className="text-lg font-semibold mb-4">Settings</h2>
-            <nav className="space-y-2">
-                <Link
-                    to="/"
-                    className={`block p-2 rounded ${location.pathname === '/' ? 'bg-blue-500 !text-white' : 'hover:bg-gray-200'}`}
-                >
-                    General Settings
-                </Link>
-                <Link
-                    to="/permissions"
-                    className={`block p-2 rounded ${location.pathname === '/permissions' ? 'bg-blue-500 !text-white' : 'hover:bg-gray-200'}`}
-                >
-                    Permissions
-                </Link>
-            </nav>
+        <div className={clsx('bg-white w-72 p-5 border-r border-r-border overflow-hidsden !transition-all')}>
+            <aside className="flex flex-col gap-y-10 sticky top-12 transition-all z-10">
+                {isOpen && (
+                    <section className="flex items-center gap-x-2.5">
+                        <div className="size-10 rounded-lg bg-primary flex items-center justify-center">
+                            <Settings className="text-white" size={21} />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <span className="text-gray-900 font-medium">WP SMS</span>
+                            <span className="text-gray-500">Settings</span>
+                        </div>
+                    </section>
+                )}
+
+                <section className="flex flex-col gap-y-5">
+                    {settingSchemaList?.data?.core && (
+                        <SidebarGroup title="Core Settings" showTitle={!!isOpen}>
+                            {Object.entries(settingSchemaList?.data?.core ?? {})?.map(([key, value]) => {
+                                return (
+                                    <SidebarItem
+                                        showTitle={!!isOpen}
+                                        key={`core-item-${value?.name}`}
+                                        href={value?.name}
+                                        icon={value?.icon}
+                                        title={value?.label}
+                                    />
+                                );
+                            })}
+                        </SidebarGroup>
+                    )}
+
+                    {settingSchemaList?.data?.addons && (
+                        <SidebarGroup title="Addons" showTitle={!!isOpen}>
+                            {Object.entries(settingSchemaList?.data?.addons ?? {})?.map(([key, value]) => {
+                                return (
+                                    <SidebarItem
+                                        showTitle={!!isOpen}
+                                        key={`addon-item-${value?.name}`}
+                                        href={value?.name}
+                                        icon={value?.icon}
+                                        title={value?.label}
+                                    />
+                                );
+                            })}
+                        </SidebarGroup>
+                    )}
+
+                    {settingSchemaList?.data?.integrations && (
+                        <SidebarGroup title="Integrations" showTitle={!!isOpen}>
+                            {Object.entries(settingSchemaList?.data?.integrations?.children ?? {})?.map(
+                                ([key, value]) => {
+                                    return (
+                                        <SidebarNestedItem
+                                            key={`integrations-item-${value?.label}-${key}`}
+                                            title={value?.label}
+                                            items={value?.children}
+                                            showTitle={!!isOpen}
+                                        />
+                                    );
+                                }
+                            )}
+                        </SidebarGroup>
+                    )}
+                </section>
+            </aside>
         </div>
     );
 };
