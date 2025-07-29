d034f4f78b0eab43be81784d2abf588fffae393f
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-29 17:00:04 +0330

    feat: save settings added to form onSubmit

--- frontend/src/components/settings/form-actions/index.tsx
+++ frontend/src/components/settings/form-actions/index.tsx
@@ -1,41 +1,71 @@
 import { Button } from '@/components/ui/button';
-import { useCallback, useMemo } from 'react';
-import { useFormContext, useWatch } from 'react-hook-form';
+import { getGroupValuesOptions, useSaveSettingsValues } from '@/models/settings';
+import { useQueryClient } from '@tanstack/react-query';
+import { useCallback } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
 
 export const SettingsFormActions: React.FC = () => {
-    const { formState, reset } = useFormContext();
+    const { formState, reset, handleSubmit, setValue } = useFormContext();
 
-    const formValues = useWatch();
+    const { name } = useParams();
 
-    const isFormDirty = useMemo(() => {
-        const defaultValues = formState.defaultValues ?? {};
+    const queryClient = useQueryClient();
 
-        for (const key in defaultValues) {
-            const defaultValue = defaultValues?.[key];
-            const currentValue = formValues[key];
+    const saveSettings = useSaveSettingsValues({
+        onSuccess: (response, variables) => {
+            for (const field in variables) {
+                queryClient.setQueryData(
+                    getGroupValuesOptions({ params: { groupName: name ?? 'general' } }).queryKey,
+                    (old: any) => {
+                        return {
+                            ...old,
+                            [field]: variables[field],
+                        };
+                    }
+                );
 
-            if ((defaultValue === null && currentValue === '') || (defaultValue === '' && currentValue === null)) {
-                continue;
+                setValue(field, variables[field]);
             }
 
-            if (defaultValue !== currentValue) {
-                return true;
+            toast.success('Settings saved successfully', {
+                position: 'top-center',
+                className: '!p-4',
+            });
+        },
+    });
+
+    const handleSave = useCallback(
+        async (values: Record<string, any>) => {
+            const valuesToSave = Object.keys(formState.dirtyFields).reduce((acc: Record<string, any>, key) => {
+                if (values.hasOwnProperty(key)) {
+                    acc[key] = values[key];
+                }
+                return acc;
+            }, {});
+
+            try {
+                await saveSettings.mutateAsync(valuesToSave);
+                reset(values);
+            } catch (error) {
+                toast.error('Failed to save settings');
             }
-        }
-        return false;
-    }, [formState.defaultValues, formValues]);
+        },
+        [formState.dirtyFields, saveSettings, reset]
+    );
 
     const handleReset = useCallback(() => {
         reset(formState.defaultValues);
     }, [reset, formState]);
 
     return (
-        <div className="flex items-center gap-x-3">
-            <Button disabled={!isFormDirty} type="submit">
+        <div className="flex items-center gap-x-3 sticky bottom-0 bg-background p-3 z-50 mt-2">
+            <Button disabled={!formState.isDirty} type="submit" onClick={handleSubmit(handleSave)}>
                 Save Changes
             </Button>
 
-            <Button disabled={!isFormDirty} type="button" variant="secondary" onClick={handleReset}>
+            <Button disabled={!formState.isDirty} type="reset" variant="secondary" onClick={handleReset}>
                 Reset
             </Button>
         </div>
