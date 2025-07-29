3b74e09d5be927b78ac9974cfa966a1b8ea20de7
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-29 16:59:29 +0330

    chore(dep): sonner dependency added for toasts

--- frontend/src/providers.tsx
+++ frontend/src/providers.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { PropsWithChildren } from 'react';
+import { Toaster } from './components/ui/sonner';
 
 const Providers: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
     const client = new QueryClient({
@@ -13,7 +14,12 @@ const Providers: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
         },
     });
 
-    return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+    return (
+        <QueryClientProvider client={client}>
+            <Toaster />
+            {children}
+        </QueryClientProvider>
+    );
 };
 
 export default Providers;
