e8825d50005679061b3040236cfa318ffefd21f5
Khakbazan <khakbazan.ali79@gmail.com>
2025-07-29 17:00:19 +0330

    feat: save setting implementation for fields with auto save

--- frontend/src/components/form/controlled-field-renderer/index.tsx
+++ frontend/src/components/form/controlled-field-renderer/index.tsx
@@ -10,6 +10,8 @@ import { useDebounce } from '@/core/hooks/useDebounce';
 import { ControlledPhone } from '../controlled-phone';
 import { ControlledTextarea } from '../controlled-textarea';
 import { ControlledNumberInput } from '../controlled-number-input';
+import { useQueryClient } from '@tanstack/react-query';
+import { getGroupValuesOptions, useSaveSettingsValues } from '@/models/settings';
 
 export const ControlledFieldRenderer: React.FC<ControlledFieldRendererProps> = ({ schema, isLoading = false }) => {
     const FieldComponentMap: Record<SchemaFieldType, React.FC<any>> = {
@@ -31,15 +33,35 @@ export const ControlledFieldRenderer: React.FC<ControlledFieldRendererProps> = (
 
     const fieldValue = useWatch({ name: schema?.key, exact: true });
 
-    const { formState } = useFormContext();
+    const { formState, setValue } = useFormContext();
 
-    const debouncedFieldValue = useDebounce(fieldValue, 1000);
+    const debouncedFieldValue = useDebounce(fieldValue, 500);
+
+    const queryClient = useQueryClient();
+
+    const saveSettings = useSaveSettingsValues({
+        onSuccess: (response, variables) => {
+            for (const field in variables) {
+                queryClient.setQueryData(
+                    getGroupValuesOptions({ params: { groupName: name ?? 'general' } }).queryKey,
+                    (old: any) => {
+                        return {
+                            ...old,
+                            [field]: variables[field],
+                        };
+                    }
+                );
+
+                setValue(field, variables[field]);
+            }
+        },
+    });
 
     useEffect(() => {
         const defaultValue = formState?.defaultValues?.[schema?.key];
 
         if (schema.auto_save_and_refresh && defaultValue !== fieldValue) {
-            console.log('saved', fieldValue);
+            saveSettings.mutate({ [schema?.key]: fieldValue });
         }
     }, [schema?.key, debouncedFieldValue, formState?.defaultValues]);
 
