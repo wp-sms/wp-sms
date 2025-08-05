b44259adcb128868427fa5ada65a877f41aa9738
Amir Pirmoradian <piramir77@gmail.com>
2025-08-05 23:18:35 +0330

    feat: add DeliveryChannelInterface and implementations for Email and SMS delivery

---
+++ src/Services/OTP/Delivery/SmsChannel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WP_SMS\Services\OTP\Delivery;
+
+use WP_SMS\Components\Sms;
+use WP_SMS\Services\OTP\Contracts\Interfaces\DeliveryChannelInterface;
+
+class SmsChannel implements DeliveryChannelInterface
+{
+    public function getKey(): string
+    {
+        return 'sms';
+    }
+
+    public function send(string $to, string $message, array $context = []): bool
+    {
+        return Sms::send([
+            'to'  => $to,
+            'msg' => $message,
+        ]);
+    }
+}
