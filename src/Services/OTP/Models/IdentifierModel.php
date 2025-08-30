8fe6afa744bf472d298f863710ca4f5582a3fb66
Amir Pirmoradian <piramir77@gmail.com>
2025-08-30 23:23:58 +0330

    refactor: rename database tables for OTP models and remove unused user_id references in MagicLinkModel and OtpSessionModel

--- src/Services/OTP/Models/IdentifierModel.php
+++ src/Services/OTP/Models/IdentifierModel.php
@@ -15,7 +15,7 @@ class IdentifierModel extends AbstractBaseModel
 
     protected static function getTableName(): string
     {
-        return static::table('identifiers');
+        return static::table('sms_identifiers');
     }
 
     /**
