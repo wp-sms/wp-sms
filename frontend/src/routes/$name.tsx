327ae8da7d8b58d0dd4cf9cb8f24584f454de4e2
Ehsan Moodi <ehsanmoodi90@gmail.com>
2025-09-03 19:24:05 +0330

    fix: admin phone number bug

--- frontend/src/routes/$name.tsx
+++ frontend/src/routes/$name.tsx
@@ -1,7 +1,68 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 
-import SettingsDynamicPages from '@/pages/settings/dynamic-pages'
+import { SettingsDynamicForm } from '@/components/settings/dynamic-form'
+import { SettingsFormActions } from '@/components/settings/form-actions'
+import { Form } from '@/components/ui/form'
+import { useStableCallback } from '@/hooks/use-stable-callback'
+import { useGetGroupSchema } from '@/services/settings/use-get-group-schema'
+import { useGetGroupValues } from '@/services/settings/use-get-group-values'
 
 export const Route = createFileRoute('/$name')({
-  component: SettingsDynamicPages,
+  component: RouteComponent,
 })
+
+function RouteComponent() {
+  const { name } = Route.useParams()
+
+  const {
+    data: groupSchema,
+    isLoading: isGroupSchemaLoading,
+    isRefetching: isGroupSchemaRefetching,
+  } = useGetGroupSchema({
+    params: {
+      groupName: name ?? 'general',
+    },
+  })
+
+  const {
+    data: groupValues,
+    isLoading: isGroupValuesLoading,
+    isRefetching: isGroupValuesRefetching,
+  } = useGetGroupValues({
+    params: {
+      groupName: name ?? 'general',
+    },
+  })
+
+  const form = useForm({
+    defaultValues: {},
+  })
+
+  const initForm = useStableCallback(async () => {
+    if (groupValues?.data && groupSchema?.data) {
+      form.reset(groupValues?.data ?? {})
+    }
+  }, [groupValues?.data, groupSchema?.data, form])
+
+  useEffect(() => {
+    initForm()
+  }, [groupValues?.data, groupSchema?.data, initForm])
+
+  return (
+    <div className="p-6">
+      <Form {...form}>
+        <div className="flex flex-col gap-y-4">
+          <SettingsDynamicForm
+            groupSchema={groupSchema?.data}
+            isInitialLoading={isGroupSchemaLoading || isGroupValuesLoading}
+            isRefreshing={isGroupSchemaRefetching || isGroupValuesRefetching}
+          />
+
+          <SettingsFormActions />
+        </div>
+      </Form>
+    </div>
+  )
+}
