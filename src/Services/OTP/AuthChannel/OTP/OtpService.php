f9f3a1bcf92377fb1855ffe6427be5fe3dd1f054
Amir Pirmoradian <piramir77@gmail.com>
2025-09-22 20:16:00 +0330

    refactor: update OTP generation method to accept variable code length for improved flexibility

--- src/Services/OTP/AuthChannel/OTP/OtpService.php
+++ src/Services/OTP/AuthChannel/OTP/OtpService.php
@@ -10,7 +10,6 @@ use WP_SMS\Services\OTP\Contracts\Interfaces\DeliveryChannelInterface;
 class OtpService implements AuthChannelInterface
 {
     protected int $defaultTtl = 300;
-    protected int $defaultCodeLength = 6;
     protected DeliveryChannelManager $channelManager;
     protected OTPChannelHelper $channelHelper;
 
@@ -28,7 +27,7 @@ class OtpService implements AuthChannelInterface
     /**
      * Generate a new OTP and persist it in the database.
      */
-    public function generate(string $flowId, string $identifier)
+    public function generate(string $flowId, string $identifier, int $otpDigits = 6)
     {
         // Validate identifier
         if (empty($identifier)) {
@@ -41,7 +40,7 @@ class OtpService implements AuthChannelInterface
         }
 
         // Generate secure OTP
-        $length = $this->defaultCodeLength;
+        $length = $otpDigits;
         $length = max(4, min(10, $length));
         $hash = bin2hex(openssl_random_pseudo_bytes(16));
         $values = array_values(unpack('C*', $hash));
