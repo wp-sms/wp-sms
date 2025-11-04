b2fa0e99b3dda031407ce351804d3f958e1d5ab9
Nora <mwdrad@gmail.com>
2025-11-04 14:00:59 +0330

    fix: fix accessibility issues

--- views/components/modals/all-in-one/step-content.php
+++ views/components/modals/all-in-one/step-content.php
@@ -53,6 +53,6 @@ if ($step_name !== 'first-step') {
             </div>
         <?php endif; ?>
     <?php else: ?>
-        <img class="wp-sms-aio-step__image v-image-lazy" width="509" height="291" data-src="<?php echo WP_SMS_URL . 'assets/images/premium-modal/first-step.png'; ?>">
+        <img class="wp-sms-aio-step__image v-image-lazy" alt="WP SMS AIO Step" width="509" height="291" data-src="<?php echo WP_SMS_URL . 'assets/images/premium-modal/first-step.png'; ?>">
     <?php endif; ?>
 </div>
