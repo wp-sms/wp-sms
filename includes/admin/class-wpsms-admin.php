d5e481397ae115137bc983a18f4acd98f721f725
mostafa <mst404@gmail.com>
2025-11-26 10:39:39 +0330

    Get rid of the fucking asterisk

--- includes/admin/class-wpsms-admin.php
+++ includes/admin/class-wpsms-admin.php
@@ -47,7 +47,7 @@ class Admin
         $screen = get_current_screen();
         if (stristr($screen->id, 'wp-sms') or $screen->base == 'post' or $screen->id == 'edit-wpsms-command' or $screen->id == 'edit-sms-campaign') {
             $text = sprintf(
-                __('Please rate <strong>WP SMS</strong> <a href="%s" aria-label="%s" title="%s" target="_blank">★★★★★ %s</a> to help us spread the word. Thank you!', 'wp-sms'),
+                __('Please rate <strong>WP SMS</strong> <a href="%s" aria-label="%s" title="%s" target="_blank">%s</a> to help us spread the word. Thank you!', 'wp-sms'),
                 'https://wordpress.org/support/plugin/wp-sms/reviews/',
                 esc_attr__('Rate WP SMS with five stars on WordPress.org', 'wp-sms'),
                 esc_attr__('Rate WP SMS', 'wp-sms'),
