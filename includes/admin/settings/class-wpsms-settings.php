6614f7a86f5dc0e9fb21e832332605a27ffed5a1
Mohammad Yari <mhd.yari021@gmail.com>
2025-12-07 13:44:17 +0330

    fix: resolve merge conflict

--- includes/admin/settings/class-wpsms-settings.php
+++ includes/admin/settings/class-wpsms-settings.php
@@ -123,6 +123,8 @@ class Settings
                 $optionId = $option['id'];
                 $readonly = (isset($option['readonly']) && $option['readonly'] == true) ? 'wpsms-pro-feature' : '';
 
+                $has_label_for = in_array($option['type'], ['text', 'select', 'textarea', 'number']);
+
                 add_settings_field(
                     "$this->setting_name[$optionId]",
                     $name,
@@ -140,7 +142,7 @@ class Settings
                         'std'         => isset($option['std']) ? $option['std'] : '',
                         'doc'         => isset($option['doc']) ? $option['doc'] : '',
                         'class'       => isset($option['className']) ? $option['className'] . " tr-{$option['type']} {$readonly} " : "tr-{$option['type']} {$readonly} ",
-                        'label_for'   => true,
+                        'label_for'   => $has_label_for ? esc_attr($this->setting_name) . '[' . esc_attr($optionId) . ']' : null,
                         'attributes'  => isset($option['attributes']) ? $option['attributes'] : [],
                     )
                 );
@@ -393,7 +395,7 @@ class Settings
                     'className'  => 'js-wpsms-show_if_international_mobile_disabled',
                     'desc'       => esc_html__('If the user\'s mobile number requires a country code, select it from the list. If the number is not specific to any country, select \'No country code (Global / Local)\'.', 'wp-sms'),
                     'options'    => array_merge(['0' => esc_html__('No country code (Global / Local)', 'wp-sms')], wp_sms_countries()->getCountriesMerged()),
-                    'attributes' => ['class' => 'js-wpsms-select2'],
+                    'attributes' => ['class' => 'js-wpsms-select2', 'aria-label' => esc_html__('Country Code Prefix', 'wp-sms')],
                 ),
                 'mobile_terms_minimum'                     => array(
                     'id'        => 'mobile_terms_minimum',
@@ -1437,7 +1439,7 @@ It might be a phone number (e.g., +1 555 123 4567) or an alphanumeric ID if supp
             $value = isset($args['std']) ? $args['std'] : '';
         }
 
-        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" class="js-wpsms-select2"/>', esc_attr($args['id']));
+        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" aria-label="' . esc_attr($this->setting_name) . '[%1$s][]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" class="js-wpsms-select2"/>', esc_attr($args['id']));
         $selected = '';
 
         foreach ($args['options'] as $k => $name) :
@@ -1466,7 +1468,7 @@ It might be a phone number (e.g., +1 555 123 4567) or an alphanumeric ID if supp
             $value = isset($args['std']) ? $args['std'] : '';
         }
 
-        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" class="js-wpsms-select2"/>', esc_attr($args['id']));
+        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" aria-label="' . esc_attr($this->setting_name) . '[%1$s][]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" class="js-wpsms-select2"/>', esc_attr($args['id']));
         $selected = '';
 
         foreach ($args['options'] as $option => $country) :
@@ -1494,8 +1496,7 @@ It might be a phone number (e.g., +1 555 123 4567) or an alphanumeric ID if supp
         }
 
         $class_name = 'js-wpsms-select2';
-        $html       = sprintf('<select class="%1$s" id="' . esc_attr($this->setting_name) . '[%2$s]" name="' . esc_attr($this->setting_name) . '[%2$s]">', esc_attr($class_name), esc_attr($args['id']));
-
+        $html       = sprintf('<select class="%1$s" id="' . esc_attr($this->setting_name) . '[%2$s]"  aria-label="' . esc_attr($this->setting_name) . '[%2$s]" name="' . esc_attr($this->setting_name) . '[%2$s]">', esc_attr($class_name), esc_attr($args['id']));
         foreach ($args['options'] as $key => $v) {
             $html .= sprintf('<optgroup data-options="" label="%1$s">', ucfirst(str_replace('_', ' ', $key)));
 
@@ -1531,7 +1532,7 @@ It might be a phone number (e.g., +1 555 123 4567) or an alphanumeric ID if supp
         }
 
 //        $class_name = 'js-wpsms-select2';
-        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" class="js-wpsms-select2"/>', esc_attr($args['id']));
+        $html     = sprintf('<select id="' . esc_attr($this->setting_name) . '[%1$s]" name="' . esc_attr($this->setting_name) . '[%1$s][]" multiple="true" aria-label="' . esc_attr($this->setting_name) . '[%1$s][]" class="js-wpsms-select2"/>', esc_attr($args['id']));
         $selected = '';
 
         foreach ($args['options'] as $k => $v) :
@@ -1676,10 +1677,11 @@ It might be a phone number (e.g., +1 555 123 4567) or an alphanumeric ID if supp
                             ));
 
                             $active            = $this->active_tab == $tab_id ? 'active' : '';
+                            $ariaDisabled      = $this->active_tab != $tab_id ? ' aria-disabled="true"' : '';
                             $isIntegrationsTab = in_array($tab_id, $this->pluginIntegrationsTabs) ? ' is-pro-tab' : '';
 
                             $tabUrl = ($tab_id == 'integrations') ? esc_url(WP_SMS_ADMIN_URL . 'admin.php?page=wp-sms-integrations') : esc_url($tab_url);
-                            echo '<li class="tab-' . esc_attr($tab_id) . esc_attr($isIntegrationsTab) . '"><a href="' . esc_url($tabUrl) . '" title="' . esc_attr($tab_name) . '" class="' . esc_attr($active) . '">';
+                            echo '<li class="tab-' . esc_attr($tab_id) . esc_attr($isIntegrationsTab) . '"><a href="' . esc_url($tabUrl) . '" title="' . esc_attr($tab_name) . '" class="' . esc_attr($active) . '"' . $ariaDisabled . '>';
                             echo esc_html($tab_name);
                             echo '</a></li>';
                         };
