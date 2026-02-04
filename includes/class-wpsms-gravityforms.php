4d1e7eb80c5f618484742133dd3817288c5d6e6c
Foad <foad.adeli@gmail.com>
2026-02-04 16:18:08 +0330

    refactor: filter non-input field types from Gravity Forms phone field dropdown
    Exclude submit, button, html, section, page, captcha, and honeypot field
    types from the phone number field selection dropdown. These are structural
    or non-input fields that cannot contain a phone number.

--- includes/class-wpsms-gravityforms.php
+++ includes/class-wpsms-gravityforms.php
@@ -9,6 +9,20 @@ if (!defined('ABSPATH')) {
 
 class Gravityforms
 {
+    /**
+     * Field types that should be excluded from phone number field selection
+     * These are non-input fields that cannot contain a phone number
+     */
+    private static $excludedFieldTypes = [
+        'submit',
+        'button',
+        'html',
+        'section',
+        'page',
+        'captcha',
+        'honeypot',
+    ];
+
     static function get_field($form_id)
     {
         $option_field = array();
@@ -25,6 +39,14 @@ class Gravityforms
 
         if ($fields) {
             foreach ($fields['fields'] as $field) {
+                // Get field type - handle both array and object access
+                $fieldType = isset($field['type']) ? $field['type'] : (isset($field->type) ? $field->type : '');
+
+                // Skip non-input fields (buttons, sections, etc.)
+                if (in_array($fieldType, self::$excludedFieldTypes, true)) {
+                    continue;
+                }
+
                 if (isset($field['label'])) {
                     $option_field[$field['id']] = $field['label'];
                 } elseif (isset($field->label)) {
