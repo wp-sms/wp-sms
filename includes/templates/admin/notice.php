74bc193ca6df7e6e290778c9e9222b3949fcc39e
mostafa <mst404@gmail.com>
2026-03-10 17:11:16 +0330

    Remove old v7 files no longer part of v8 architecture
    Clean up all legacy includes/, gateways/, templates/, old tests,
    vendor CSS/JS, and other v7 artifacts that were replaced by the
    new src/ and compat/ structure.

--- includes/templates/admin/notice.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('ABSPATH')) exit; // Exit if accessed directly ?>
-<div class="notice notice-<?php echo esc_attr($type); ?> wpsms-admin-notice<?php echo $dismiss ? ' wpsms-admin-notice__dismissible' : ''; ?>">
-    <p><?php echo wp_kses_post($message); ?></p>
-    <?php if ($dismiss) : ?>
-        <a href="<?php echo esc_url($link); ?>" class="notice-dismiss">
-            <span class="screen-reader-text"><?php echo esc_html__('Dismiss this notice.', 'wp-sms'); ?></span>
-        </a>
-    <?php endif; ?>
-</div>
--- security.md
+++
@@ -1,6 +0,0 @@
-# Security Policy
-## Reporting Security Bugs
-
-#### How can I report security bugs?
-
-You can report security bugs through the Patchstack Vulnerability Disclosure Program. The Patchstack team help validate, triage and handle any security vulnerabilities. [Report a security vulnerability.](https://patchstack.com/database/vdp/wp-sms)
